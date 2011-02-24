3a7e670ca86c33443e19f9b39b0d994231d87822
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2011-02-24 16:03:50 -0600

    removed bad methods NokogiriDatastream.solrize_accessor and NokogiriDatastream.solrize_node.   Should rely on Solrizer Extractors instead

--- lib/active_fedora/nokogiri_datastream.rb
+++ lib/active_fedora/nokogiri_datastream.rb
@@ -97,46 +97,7 @@ class ActiveFedora::NokogiriDatastream < ActiveFedora::Datastream
     #just initialize internal_solr_doc since any value retrieval will be done via lazy loading on this doc on-demand
     @internal_solr_doc = solr_doc
   end
-  
-  def solrize_accessor(accessor_name, accessor_info, opts={})
-    solr_doc = opts.fetch(:solr_doc, Solr::Document.new)
-    parents = opts.fetch(:parents, [])
-    
-    accessor_pointer = parents+[accessor_name]
-  
-    if accessor_info.nil?
-      accessor_info = self.class.accessor_info(accessor_pointer)
-      if accessor_info.nil?
-        raise "No accessor is defined for #{accessor_info.select}"
-      end
-    end
-  
-    # prep children hash
-    child_accessors = accessor_info.fetch(:children, {})
-    xpath = self.class.accessor_xpath(*accessor_pointer)
-    nodeset = lookup(xpath)
-    
-    nodeset.each do |node|
-      # create solr fields
-      solrize_node(node, accessor_pointer, solr_doc)
-      child_accessors.each_pair do |child_accessor_name, child_accessor_info|
-        solrize_accessor(child_accessor_name, child_accessor_info, opts={:solr_doc=>solr_doc, :parents=>parents+[{accessor_name=>nodeset.index(node)}] })
-      end
-    end
-  end
-  
-  def solrize_node(node, accessor_pointer, solr_doc = Solr::Document.new)
-    generic_field_name_base = self.class.accessor_generic_name(*accessor_pointer)
-    generic_field_name = ActiveFedora::SolrService.solr_name(generic_field_name_base, :text)
-    
-    solr_doc << Solr::Field.new(generic_field_name => node.text)
-    
-    if accessor_pointer.length > 1
-      hierarchical_field_name_base = self.class.accessor_hierarchical_name(*accessor_pointer)
-      hierarchical_field_name = ActiveFedora::SolrService.solr_name(hierarchical_field_name_base, :text)
-      solr_doc << Solr::Field.new(hierarchical_field_name => node.text)
-    end
-  end
+
 
   # ** Experimental **
   # This method is called by +get_values+ if this datastream has been initialized by calling from_solr method via
