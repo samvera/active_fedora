635043eb6ae4c8eaae74ec38f99429d90c16e6c0
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2011-05-12 09:53:38 -0500

    another fix for hudson-specific load-time bug

--- lib/active_fedora/samples/special_thing.rb
+++ lib/active_fedora/samples/special_thing.rb
@@ -1,5 +1,7 @@
 require "active-fedora"
-require "active_fedora/samples"
+require "active_fedora/samples/hydra-mods_article_datastream.rb"
+require "active_fedora/samples/hydra-rights_metadata_datastream.rb"
+require "active_fedora/samples/marpa-dc_datastream.rb"
 
 # This is an example of an ActiveFedora Model
 #
