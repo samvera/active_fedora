ffc1ebb367db35816d9bf40524b9872f38175fd5
Justin Coyne <justin.coyne@yourmediashelf.com>
2011-09-23 09:22:23 -0500

    Master is now development for 3.1.x

--- spec/unit/rels_ext_datastream_spec.rb
+++ spec/unit/rels_ext_datastream_spec.rb
@@ -81,6 +81,29 @@ describe ActiveFedora::RelsExtDatastream do
       @test_obj.save
       @test_obj.relationships.should == {:self=>{:is_part_of=>["info:fedora/demo:11"], "foo"=>["info:fedora/foo:bar"], :has_model=>["info:fedora/afmodel:ActiveFedora_Base"], :is_member_of=>["info:fedora/demo:10"], :conforms_to=>["AnInterface"]}}
     end
+    it "should handle un-mapped literals" do
+      pending
+      xml = "
+              <foxml:datastream ID=\"RELS-EXT\" STATE=\"A\" CONTROL_GROUP=\"X\" VERSIONABLE=\"true\" xmlns:foxml=\"info:fedora/fedora-system:def/foxml#\">
+              <foxml:datastreamVersion ID=\"RELS-EXT.0\" LABEL=\"\" CREATED=\"2011-09-20T19:48:43.714Z\" MIMETYPE=\"text/xml\" SIZE=\"622\">
+                <foxml:xmlContent>
+                <rdf:RDF xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\">
+                  <rdf:Description rdf:about=\"info:fedora/changeme:3489\">
+                    <hasModel xmlns=\"info:fedora/fedora-system:def/model#\" rdf:resource=\"info:fedora/afmodel:ActiveFedora_Base\"/>
+                    <isPartOf xmlns=\"info:fedora/fedora-system:def/relations-external#\" rdf:resource=\"info:fedora/demo:11\"/>
+                    <isMemberOf xmlns=\"info:fedora/fedora-system:def/relations-external#\" rdf:resource=\"info:fedora/demo:10\"/>
+                    <hasMetadata xmlns=\"info:fedora/fedora-system:def/relations-external#\">oai:hull.ac.uk:hull:2708</hasMetadata>
+                  </rdf:Description>
+                </rdf:RDF>
+              </foxml:xmlContent>
+            </foxml:datastreamVersion>\n</foxml:datastream>\n"
+      doc = Nokogiri::XML::Document.parse(xml)
+      new_ds = ActiveFedora::RelsExtDatastream.new
+      ActiveFedora::RelsExtDatastream.from_xml(new_ds,doc.root)
+      new_ext = new_ds.to_rels_ext('changeme:3489')
+      new_ext.should match "<hasMetadata xmlns=\"info:fedora/fedora-system:def/relations-external#\">oai:hull.ac.uk:hull:2708</hasMetadata>"
+      
+    end
   end
   
   
