ad850f05a7b7a190421fa4f08196e922c0237230
Justin Coyne <justin.coyne@yourmediashelf.com>
2011-10-19 06:53:23 -0500

    Added DigitalObject to the repo

---
+++ lib/active_fedora/digital_object.rb
@@ -0,0 +1,18 @@
+module ActiveFedora
+  # Helps Rubydora create datastreams of the type defined by the ActiveFedora::Base#datastream_class_for_name
+  class DigitalObject < Rubydora::DigitalObject
+    attr_accessor :original_class
+    
+    def self.find(original_class, pid)
+      obj = super(pid, RubydoraConnection.instance.connection)
+      obj.original_class = original_class
+      obj
+    end
+
+    def datastream_object_for dsid
+      klass = original_class.datastream_class_for_name(dsid)
+      klass.new self, dsid
+    end
+    
+  end
+end
