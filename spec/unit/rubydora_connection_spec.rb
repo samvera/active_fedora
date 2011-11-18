101a0905bf2ea307de8cdf40f4c38ca6a6df04d7
Michael Klein <mbklein@gmail.com>
2011-11-17 21:29:43 -0800

    Added tests for RubydoraConnection option passing and forced reconnection

--- spec/unit/rubydora_connection_spec.rb
+++ spec/unit/rubydora_connection_spec.rb
@@ -16,6 +16,35 @@ describe ActiveFedora::RubydoraConnection do
     end
   end
 
+  describe 'connect' do
+    before do
+      @instance = ActiveFedora::RubydoraConnection.instance
+      @reconfig = { :force => true, :url => @instance.connection.client.url }.merge(@instance.connection.client.options)
+    end
+    
+    after do
+      ActiveFedora::RubydoraConnection.connect @reconfig
+    end
+    
+    it "shouldn't reconnect by default" do
+      client_id = @instance.connection.client.object_id
+      ActiveFedora::RubydoraConnection.connect :timeout => 3600
+      @instance.connection.client.object_id.should == client_id
+    end
+    
+    it "should reconnect with force" do
+      client_id = @instance.connection.client.object_id
+      ActiveFedora::RubydoraConnection.connect :force => true
+      @instance.connection.client.object_id.should_not == client_id
+    end
+    
+    it "should pass through valid options" do
+      ActiveFedora::RubydoraConnection.connect :timeout => 3600, :fake_option => :missing, :force => true
+      @instance.connection.client.options[:timeout].should == 3600
+      @instance.connection.client.options.has_key?(:fake_option).should be_false
+    end
+  end
+
   describe 'find_model' do
     
   end
