c5a52a42ab81ac7ea1093f7ba0d05785f8e94301
Benjamin Armintor <armintor@gmail.com>
2011-12-09 18:11:04 -0500

    building sdef methods on objects, tests except block passed to rubydora

--- lib/active_fedora.rb
+++ lib/active_fedora.rb
@@ -30,6 +30,7 @@ module ActiveFedora #:nodoc:
     autoload :QualifiedDublinCoreDatastream
     autoload :RelsExtDatastream
     autoload :RelationshipsHelper
+    autoload :ServiceDefinitions
     autoload :SemanticNode
     autoload :NestedAttributes
     autoload :FixtureLoader
