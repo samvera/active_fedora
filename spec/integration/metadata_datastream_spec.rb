f6d5f8f90c2ea84aaba361b29168399beb816bf2
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-01-20 10:58:32 -0600

    Better looking test

--- spec/integration/metadata_datastream_spec.rb
+++ spec/integration/metadata_datastream_spec.rb
@@ -12,24 +12,28 @@ describe ActiveFedora::MetadataDatastream do
           m.field "alt_title", :string
         end
       end
-      @obj = Foo.new()
-      @obj.properties.update_indexed_attributes({ [:field1] => {"0" => "test value"}} ) 
-      @obj.stuff.update_indexed_attributes({ [:alt_title] => {"0" => "Title"}} ) 
+      obj = Foo.new()
+      obj.properties.update_indexed_attributes({ [:field1] => {"0" => "test value"}} ) 
+      obj.stuff.update_indexed_attributes({ [:alt_title] => {"0" => "Title"}} ) 
+      obj.save
 
-      @obj.save
+      #Update the object
+      obj2 = Foo.find(obj.pid)
+      obj2.stuff.update_indexed_attributes({ [:alt_title] => {"0" => "Moo Cow"}} )
+      obj2.save
+
+      @obj = Foo.find(obj.pid)
     end
 
     after do
       Object.send(:remove_const, :Foo)
     end
 
-    it "should have the values" do
-      obj = Foo.find(@obj.pid)
-      obj.stuff.update_indexed_attributes({ [:alt_title] => {"0" => "Moo Cow"}} )
-      obj.save
-      obj2 = Foo.find(@obj.pid)
-      obj2.stuff.alt_title_values.should == ['Moo Cow']
-      obj2.properties.field1_values.should == ['test value']
+    it "should have updated the one datastream" do
+      @obj.stuff.alt_title_values.should == ['Moo Cow']
+    end
+    it "should not have changed the other datastream" do
+      @obj.properties.field1_values.should == ['test value']
     end
   end
 end
