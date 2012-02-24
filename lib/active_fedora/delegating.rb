28a0c792ffca1bc2cb37dffdd5f49807e36f8024
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2012-02-24 18:03:36 -0500

    RDFDatastream now functioning as expected w/ all tests passing, making use of func already provided by AF::Predicates. removed DCRDFDatastream. still quite ugly; lots of puts debug statements to remove; lots of commented out crud. will clean up next.

--- lib/active_fedora/delegating.rb
+++ lib/active_fedora/delegating.rb
@@ -1,7 +1,7 @@
 module ActiveFedora
   module Delegating
     extend ActiveSupport::Concern
-
+    
     module ClassMethods
       # Provides a delegate class method to expose methods in metadata streams
       # as member of the base object. Pass the target datastream via the 
@@ -26,33 +26,34 @@ module ActiveFedora
         create_delegate_accessor(field, args)
         create_delegate_setter(field, args)
       end
-
+      
       private
-        def create_delegate_accessor(field, args)
-            define_method field do
-              ds = self.send(args[:to])
-              val = if ds.kind_of?(ActiveFedora::MetadataDatastream) || ds.kind_of?(ActiveFedora::DCRDFDatastream)
-                ds.send(:get_values, field)
-              else 
-                terminology = args[:at] || [field]
-                ds.send(:term_values, *terminology)
-              end 
-              args[:unique] ? val.first : val
-                
-            end
+      def create_delegate_accessor(field, args)
+        define_method field do
+          ds = self.send(args[:to])
+          val = if ds.kind_of?(ActiveFedora::MetadataDatastream) || ds.kind_of?(ActiveFedora::RDFDatastream)
+                  #puts "sending #{field.inspect} to g_v"
+                  ds.send(:get_values, field)
+                else 
+                  terminology = args[:at] || [field]
+                  ds.send(:term_values, *terminology)
+                end
+          #puts "val: #{val.inspect}"
+          args[:unique] ? val.first : val
         end
-
-        def create_delegate_setter(field, args)
-            define_method "#{field}=".to_sym do |v|
-              ds = self.send(args[:to])
-              if ds.kind_of?(ActiveFedora::MetadataDatastream) || ds.kind_of?(ActiveFedora::DCRDFDatastream)
-                ds.send(:set_value, field, v)
-              else 
-                terminology = args[:at] || [field]
-                ds.send(:update_indexed_attributes, {terminology => v})
-              end
-            end
+      end
+      
+      def create_delegate_setter(field, args)
+        define_method "#{field}=".to_sym do |v|
+          ds = self.send(args[:to])
+          if ds.kind_of?(ActiveFedora::MetadataDatastream) || ds.kind_of?(ActiveFedora::RDFDatastream)
+            ds.send(:set_value, field, v)
+          else 
+            terminology = args[:at] || [field]
+            ds.send(:update_indexed_attributes, {terminology => v})
+          end
         end
+      end
     end
   end
 end
