4b6fad8bfe645eb5729707571221ac2dbd2ef677
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2012-02-24 18:03:36 -0500

    RDFDatastream now functioning as expected w/ all tests passing, making use of func already provided by AF::Predicates. removed DCRDFDatastream. still quite ugly; lots of puts debug statements to remove; lots of commented out crud. will clean up next.

--- spec/integration/ntriples_datastream_spec.rb
+++ spec/integration/ntriples_datastream_spec.rb
@@ -2,17 +2,16 @@ require 'spec_helper'
 
 describe ActiveFedora::NtriplesRDFDatastream do
   before do
-    class NtriplesMetadataDatastream < ActiveFedora::NtriplesRDFDatastream
-      include ActiveFedora::RDFDatastream::ModelMethods
-      register_vocabularies RDF::DC, RDF::FOAF, RDF::RDFS
+    class MyDatastream < ActiveFedora::NtriplesRDFDatastream
+      #register_vocabularies RDF::DC, RDF::FOAF, RDF::RDFS
       map_predicates do |map|
-        map.title(:to => "title", :in => RDF::DC)
-        map.based_near(:to => "based_near", :in => RDF::FOAF)
+        map.title(:in => RDF::DC)
+        map.based_near(:in => RDF::FOAF)
         map.related_url(:to => "seeAlso", :in => RDF::RDFS)
       end
     end
     class RdfTest < ActiveFedora::Base 
-      has_metadata :name=>'rdf', :type=>NtriplesMetadataDatastream
+      has_metadata :name=>'rdf', :type=>MyDatastream
       delegate :based_near, :to=>'rdf'
       delegate :related_url, :to=>'rdf'
       delegate :title, :to=>'rdf', :unique=>true
@@ -22,7 +21,7 @@ describe ActiveFedora::NtriplesRDFDatastream do
 
   after do
     Object.send(:remove_const, :RdfTest)
-    Object.send(:remove_const, :NtriplesMetadataDatastream)
+    Object.send(:remove_const, :MyDatastream)
   end
 
   it "should set and recall values" do
@@ -33,7 +32,7 @@ describe ActiveFedora::NtriplesRDFDatastream do
 
     loaded = RdfTest.find(@subject.pid)
     loaded.title.should == 'War and Peace'
-    loaded.based_near.should == 'Moscow, Russia'
-    loaded.related_url.should == 'http://en.wikipedia.org/wiki/War_and_Peace'
+    loaded.based_near.should == ['Moscow, Russia']
+    loaded.related_url.should == ['http://en.wikipedia.org/wiki/War_and_Peace']
   end
 end
