b6c5171b41939e604b484f24c02be82eed3641e9
Chris Beer <cabeer@stanford.edu>
2012-07-20 16:06:14 -0700

    add an after_rollback handler to update the SolrService when an object is rolled back

--- lib/active_fedora/rubydora_connection.rb
+++ lib/active_fedora/rubydora_connection.rb
@@ -17,6 +17,22 @@ module ActiveFedora
       client_options = options.reject { |k,v| not allowable_options.include?(k) }
       #puts "CLIENT OPTS #{client_options.inspect}"
       @connection = Rubydora.connect client_options
+
+      Rubydora::Transaction.after_rollback do |options|
+        begin
+          case options[:method]
+            when :ingest
+              solr = ActiveFedora::SolrService.instance.conn
+              solr.delete_by_id(options[:pid])
+              solr.commit
+            else
+              ActiveFedora::Base.find(options[:pid], :cast=>true).update_index
+          end
+        rescue
+          # no-op
+        end
+
+      end
     end
   end
 end
