59c791f780284e5ed88482d0b99c7c67c031a2ee
Benjamin Armintor <armintor@gmail.com>
2012-07-29 22:16:57 -0400

    probably want to add :versionable to add_file_datastream, too

--- lib/active_fedora/datastreams.rb
+++ lib/active_fedora/datastreams.rb
@@ -207,6 +207,7 @@ module ActiveFedora
         attrs.merge!({:mimeType=>opts[:content_type]})
       end
       attrs[:checksumType] = opts[:checksumType] if opts[:checksumType]
+      attrs[:versionable] = opts[:versionable] unless opts[:versionable].nil?
       ds = create_datastream(self.class.datastream_class_for_name(opts[:dsid]), opts[:dsid], attrs)
       add_datastream(ds)
     end
