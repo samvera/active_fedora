33dea2662d4383ae46940b33be18844a5623d05c
Justin Coyne <justin.coyne@yourmediashelf.com>
2012-10-10 09:02:53 -0500

    HYDRA-848 Fixed create!()

---
+++ spec/integration/persistence_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe "persisting objects" do
+  before :all do
+    class MockAFBaseRelationship < ActiveFedora::Base
+      has_metadata :type=>ActiveFedora::SimpleDatastream, :name=>"foo" do |m|
+        m.field "name", :string
+      end
+      delegate :name, :to=>'foo', :unique=>true
+      validates :name, :presence=>true
+    end
+  end
+  after :all do
+    Object.send(:remove_const, :MockAFBaseRelationship)
+  end
+
+  describe "#create!" do
+    it "should validate" do
+      lambda { MockAFBaseRelationship.create!}.should raise_error ActiveFedora::RecordInvalid, "Validation failed: Name can't be blank"
+    end
+  end
+end
