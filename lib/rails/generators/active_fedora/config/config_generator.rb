b860ce9a976ecded11040dcfcad44977822829f2
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-12-03 14:59:27 -0800

    Adding ERB eval to generated configs

--- lib/rails/generators/active_fedora/config/config_generator.rb
+++ lib/rails/generators/active_fedora/config/config_generator.rb
@@ -3,12 +3,9 @@ require 'rails/generators'
 module ActiveFedora
   class ConfigGenerator < Rails::Generators::Base
     source_root File.expand_path('../templates', __FILE__)
-    def create_solr_yml
-      template('solr.yml', 'config/solr.yml')
-    end
-
-    def create_fedora_yml
-      template('fedora.yml', 'config/fedora.yml')
+    def create_configuration_files
+      copy_file('solr.yml', 'config/solr.yml')
+      copy_file('fedora.yml', 'config/fedora.yml')
     end
   end
 end
