8cf65965d0ca90b80c5e932dd1858308680b1e56
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-12-03 16:21:07 -0800

    Updating generators as Config is not a module

--- lib/rails/generators/active_fedora/config/fedora/fedora_generator.rb
+++ lib/rails/generators/active_fedora/config/fedora/fedora_generator.rb
@@ -1,13 +1,11 @@
 require 'rails/generators'
 
 module ActiveFedora
-  module Config
-    class FedoraGenerator < Rails::Generators::Base
-      source_root File.expand_path('../templates', __FILE__)
+  class Config::FedoraGenerator < Rails::Generators::Base
+    source_root File.expand_path('../templates', __FILE__)
 
-      def copy_fedora_yml
-        copy_file('fedora.yml', 'config/fedora.yml')
-      end
+    def copy_fedora_yml
+      copy_file('fedora.yml', 'config/fedora.yml')
     end
   end
 end
--- lib/rails/generators/active_fedora/config/solr/solr_generator.rb
+++ lib/rails/generators/active_fedora/config/solr/solr_generator.rb
@@ -1,13 +1,11 @@
 require 'rails/generators'
 
 module ActiveFedora
-  module Config
-    class SolrGenerator < Rails::Generators::Base
-      source_root File.expand_path('../templates', __FILE__)
+  class Config::SolrGenerator < Rails::Generators::Base
+    source_root File.expand_path('../templates', __FILE__)
 
-      def copy_fedora_yml
-        copy_file('solr.yml', 'config/solr.yml')
-      end
+    def copy_fedora_yml
+      copy_file('solr.yml', 'config/solr.yml')
     end
   end
 end
