67fd090b8ddce49a800042a2ca7e01b646d17831
Justin Coyne <justin.coyne@yourmediashelf.com>
2013-01-27 00:20:27 -0600

    Added writing methods for rdf_list

--- spec/unit/rdf_list_spec.rb
+++ spec/unit/rdf_list_spec.rb
@@ -38,70 +38,134 @@ describe ActiveFedora::RdfList do
     Object.send(:remove_const, :MADS)
   end
 
-  subject do
-    subject = DemoList.new(stub('inner object', :pid=>'foo', :new? =>true), 'descMetadata')
-    subject.content =<<END
-<rdf:RDF
-    xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:mads="http://www.loc.gov/mads/rdf/v1#">
-    
-      <mads:ComplexSubject rdf:about="info:fedora/foo">
-        <mads:elementList rdf:parseType="Collection">
-          <rdf:Description rdf:about="http://library.ucsd.edu/ark:/20775/bbXXXXXXX6"/>
-          <mads:TopicElement>
-            <mads:elementValue>Relations with Mexican Americans</mads:elementValue>
-          </mads:TopicElement>
-          <rdf:Description rdf:about="http://library.ucsd.edu/ark:/20775/bbXXXXXXX4"/>
-          <mads:TemporalElement>
-            <mads:elementValue>20th century</mads:elementValue>
-          </mads:TemporalElement>
-        </mads:elementList>
-      </mads:ComplexSubject>
-    </rdf:RDF>
+  describe "a new list" do
+    let (:ds) { DemoList.new(stub('inner object', :pid=>'foo', :new? =>true), 'descMetadata')}
+    subject { ds.elementList.build}
+
+    it "should insert at the end" do
+      subject.should be_kind_of DemoList::List
+      subject.size.should == 0
+      subject[1] = DemoList::List::TopicElement.new(subject.graph)
+      subject.size.should == 2
+    end
+
+    it "should insert at the head" do
+      subject.should be_kind_of DemoList::List
+      subject.size.should == 0
+      subject[0] = DemoList::List::TopicElement.new(subject.graph)
+      subject.size.should == 1
+    end
+
+    describe "that has 4 elements" do
+      before do
+        subject[3] = DemoList::List::TopicElement.new(subject.graph)
+        subject.size.should == 4
+      end
+      it "should insert in the middle" do
+        subject[1] = DemoList::List::TopicElement.new(subject.graph)
+        subject.size.should == 4
+      end
+    end
+
+    describe "return updated xml" do
+      it "should be built" do
+        subject[0] = RDF::URI.new "http://library.ucsd.edu/ark:/20775/bbXXXXXXX6"
+        subject[1] = DemoList::List::TopicElement.new(ds.graph)
+        subject[1].elementValue = "Relations with Mexican Americans"
+        subject[2] = RDF::URI.new "http://library.ucsd.edu/ark:/20775/bbXXXXXXX4"
+        subject[3] = DemoList::List::TemporalElement.new(ds.graph)
+        subject[3].elementValue = "20th century"
+        expected_xml =<<END
+  <rdf:RDF
+      xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:mads="http://www.loc.gov/mads/rdf/v1#">
+      
+        <rdf:Description rdf:about="info:fedora/foo">
+          <mads:elementList rdf:parseType="Collection">
+            <rdf:Description rdf:about="http://library.ucsd.edu/ark:/20775/bbXXXXXXX6"/>
+            <mads:TopicElement>
+              <mads:elementValue>Relations with Mexican Americans</mads:elementValue>
+            </mads:TopicElement>
+            <rdf:Description rdf:about="http://library.ucsd.edu/ark:/20775/bbXXXXXXX4"/>
+            <mads:TemporalElement>
+              <mads:elementValue>20th century</mads:elementValue>
+            </mads:TemporalElement>
+          </mads:elementList>
+        </rdf:Description>
+      </rdf:RDF>
+END
+        ds.content.should be_equivalent_to expected_xml
+      end
+    end
+
+
+  end
+
+  describe "a list with content" do
+    subject do
+      subject = DemoList.new(stub('inner object', :pid=>'foo', :new? =>true), 'descMetadata')
+      subject.content =<<END
+  <rdf:RDF
+      xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:mads="http://www.loc.gov/mads/rdf/v1#">
+      
+        <mads:ComplexSubject rdf:about="info:fedora/foo">
+          <mads:elementList rdf:parseType="Collection">
+            <rdf:Description rdf:about="http://library.ucsd.edu/ark:/20775/bbXXXXXXX6"/>
+            <mads:TopicElement>
+              <mads:elementValue>Relations with Mexican Americans</mads:elementValue>
+            </mads:TopicElement>
+            <rdf:Description rdf:about="http://library.ucsd.edu/ark:/20775/bbXXXXXXX4"/>
+            <mads:TemporalElement>
+              <mads:elementValue>20th century</mads:elementValue>
+            </mads:TemporalElement>
+          </mads:elementList>
+        </mads:ComplexSubject>
+      </rdf:RDF>
 END
 
-    subject
-  end
- it "should have a subject" do
-    subject.rdf_subject.to_s.should == "info:fedora/foo"
-  end
-  it "should have fields" do
-    list = subject.elementList.first
-    list.first.should == "http://library.ucsd.edu/ark:/20775/bbXXXXXXX6"
-    list[1].should be_kind_of DemoList::List::TopicElement
-    list[1].elementValue.should == ["Relations with Mexican Americans"]
-    list[2].should == "http://library.ucsd.edu/ark:/20775/bbXXXXXXX4"
-    list[3].should be_kind_of DemoList::List::TemporalElement
-    list[3].elementValue.should == ["20th century"]
-  end
+      subject
+    end
+   it "should have a subject" do
+      subject.rdf_subject.to_s.should == "info:fedora/foo"
+    end
+    it "should have fields" do
+      list = subject.elementList.first
+      list.first.should == "http://library.ucsd.edu/ark:/20775/bbXXXXXXX6"
+      list[1].should be_kind_of DemoList::List::TopicElement
+      list[1].elementValue.should == ["Relations with Mexican Americans"]
+      list[2].should == "http://library.ucsd.edu/ark:/20775/bbXXXXXXX4"
+      list[3].should be_kind_of DemoList::List::TemporalElement
+      list[3].elementValue.should == ["20th century"]
+    end
 
-  it "should have size" do
-    list = subject.elementList.first
-    list.size.should == 4
-  end
+    it "should have size" do
+      list = subject.elementList.first
+      list.size.should == 4
+    end
 
-  it "should update fields" do
-    list = subject.elementList.first
-    list[3].elementValue = ["1900s"]
-    expected_xml =<<END
-<rdf:RDF
-    xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:mads="http://www.loc.gov/mads/rdf/v1#">
-    
-      <mads:ComplexSubject rdf:about="info:fedora/foo">
-        <mads:elementList rdf:parseType="Collection">
-          <rdf:Description rdf:about="http://library.ucsd.edu/ark:/20775/bbXXXXXXX6"/>
-          <mads:TopicElement>
-            <mads:elementValue>Relations with Mexican Americans</mads:elementValue>
-          </mads:TopicElement>
-          <rdf:Description rdf:about="http://library.ucsd.edu/ark:/20775/bbXXXXXXX4"/>
-          <mads:TemporalElement>
-            <mads:elementValue>1900s</mads:elementValue>
-          </mads:TemporalElement>
-        </mads:elementList>
-      </mads:ComplexSubject>
-    </rdf:RDF>
+    it "should update fields" do
+      list = subject.elementList.first
+      list[3].elementValue = ["1900s"]
+      expected_xml =<<END
+  <rdf:RDF
+      xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:mads="http://www.loc.gov/mads/rdf/v1#">
+      
+        <mads:ComplexSubject rdf:about="info:fedora/foo">
+          <mads:elementList rdf:parseType="Collection">
+            <rdf:Description rdf:about="http://library.ucsd.edu/ark:/20775/bbXXXXXXX6"/>
+            <mads:TopicElement>
+              <mads:elementValue>Relations with Mexican Americans</mads:elementValue>
+            </mads:TopicElement>
+            <rdf:Description rdf:about="http://library.ucsd.edu/ark:/20775/bbXXXXXXX4"/>
+            <mads:TemporalElement>
+              <mads:elementValue>1900s</mads:elementValue>
+            </mads:TemporalElement>
+          </mads:elementList>
+        </mads:ComplexSubject>
+      </rdf:RDF>
 
 END
-    subject.content.should be_equivalent_to expected_xml
+      subject.content.should be_equivalent_to expected_xml
+    end
   end
 end
 
