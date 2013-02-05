499c679f560847fe5e2fb77678e8774007ed9b45
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-02-05 16:41:44 -0500

    Adding #match_fedora_datastream rspec matcher
    Given that ActiveFedora is an adaptor for CRUD operations on Fedora, I
    want to provide Rspec matchers that can greatly assist in validating
    that data written to Fedora is written how you are expecting it to be
    written.
    
    Adding have_predicate matcher
    
    Adding additional specs for have_predicate
    
    Adding #has_many_associated_active_fedora_objects
    
    Adding rspec matchers for belongs_to
    
    Updating bits of documentation for rspec helper
    
    Finalizing method for including rspec_matcher

---
+++ spec/unit/rspec_matchers/belong_to_associated_active_fedora_object_matcher_spec.rb
@@ -0,0 +1,44 @@
+require "spec_helper"
+require 'ostruct'
+require 'webmock/rspec'
+require 'lib/active_fedora/rspec_matchers/belong_to_associated_active_fedora_object_matcher'
+
+describe RSpec::Matchers, "belong_to_associated_active_fedora_object_matcher" do
+  subject { OpenStruct.new(:pid => pid )}
+  let(:pid) { 123 }
+  let(:object1) { Object.new }
+  let(:object2) { Object.new }
+  let(:association) { :association }
+
+  before(:each) do
+    subject.class.should_receive(:find).with(pid).and_return(subject)
+  end
+
+  it 'should match when association is properly stored in fedora' do
+    subject.should_receive(association).and_return(object1)
+    subject.should belong_to_associated_active_fedora_object(association).with_object(object1)
+  end
+
+  it 'should not match when association is different' do
+    subject.should_receive(association).and_return(object1)
+    lambda {
+      subject.should belong_to_associated_active_fedora_object(association).with_object(object2)
+    }.should (
+      raise_error(
+        RSpec::Expectations::ExpectationNotMetError,
+        /expected #{subject.class} PID=#{pid} association: #{association.inspect}/
+      )
+    )
+  end
+
+  it 'should require :with_object option' do
+    lambda {
+      subject.should belong_to_associated_active_fedora_object(association)
+    }.should(
+      raise_error(
+        ArgumentError,
+        "subject.should belong_to_associated_active_fedora_object(<association_name>).with_object(<object>)"
+      )
+    )
+  end
+end
