526012c70e55b711fd3b4a731159385a726680ce
Justin Coyne <justin.coyne@yourmediashelf.com>
2013-02-15 13:10:33 -0600

    Use ActiveSupport::Deprecation.warn, not Deprecation.warn

--- lib/active_fedora/nokogiri_datastream.rb
+++ lib/active_fedora/nokogiri_datastream.rb
@@ -2,7 +2,7 @@
 module ActiveFedora
   class NokogiriDatastream < OmDatastream
     def initialize(digital_object=nil, dsid=nil, options={})
-      Deprecation.warn("NokogiriDatastream is deprecated and will be removed in active-fedora 7.0, use OmDatastream instead", caller)
+      ActiveSupport::Deprecation.warn("NokogiriDatastream is deprecated and will be removed in active-fedora 7.0, use OmDatastream instead", caller)
       super
     end
   end
