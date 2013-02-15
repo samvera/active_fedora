046b6bc2511feef0b512b6fd59a168294b4d2e32
Justin Coyne <justin.coyne@yourmediashelf.com>
2013-02-15 13:27:25 -0600

    Use Nokogiri for assign pid

--- lib/active_fedora/base.rb
+++ lib/active_fedora/base.rb
@@ -214,8 +214,8 @@ module ActiveFedora
         credentials = ActiveFedora.config.credentials
       end
       fedora_connection[0] ||= ActiveFedora::RubydoraConnection.new(credentials)
-      d = REXML::Document.new( fedora_connection[0].connection.next_pid(args))
-      pid =d.elements['//pid'].text
+      d = Nokogiri::XML( fedora_connection[0].connection.next_pid(args))
+      pid = d.xpath('//fedora:pid', 'fedora' => 'http://www.fedora.info/definitions/1/0/management/').text
       pid
     end
 
