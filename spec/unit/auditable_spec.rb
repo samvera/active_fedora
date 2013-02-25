75480079432f63941f8479f3116217e5cc9c540d
dchandekstark <dchandekstark@gmail.com>
2013-02-25 13:40:41 -0500

    made fixture load work

--- spec/unit/auditable_spec.rb
+++ spec/unit/auditable_spec.rb
@@ -2,29 +2,29 @@ require 'spec_helper'
 
 describe ActiveFedora::Auditable do
 
-  before do
+  before(:all) do
     class AuditableModel < ActiveFedora::Base
-      extends ActiveFedora::FixtureLoader
       include ActiveFedora::Auditable
     end
-    @pid = "test:auditable"
-    AuditableModel.import_to_fedora("#{Rails.root}/spec/fixtures/changeme155.xml", @pid)
-    @test_object = AuditableModel.find(@pid)
+    path = File.join(File.dirname(__FILE__), '..', 'fixtures', 'auditable.foxml.xml')
+    pid = ActiveFedora::FixtureLoader.import_to_fedora(path)
+    ActiveFedora::FixtureLoader.index(pid)
+    @test_object = AuditableModel.find(pid)
   end
-  after do
+  after(:all) do
     @test_object.delete
   end
   it "should have the correct number of audit records" do
-    @test_object.audit_trail.records.length.should == 14
+    @test_object.audit_trail.records.length.should == 3
   end
   it "should return all the data from each audit record" do
-    record = @test_object.audit_trail.records.last
-    record.id.should == "AUDREC14"
+    record = @test_object.audit_trail.records.first
+    record.id.should == "AUDREC1"
     record.process_type.should == "Fedora API-M"
     record.action.should == "addDatastream"
-    record.component_id.should == "dublin_core"
+    record.component_id.should == "RELS-EXT"
     record.responsibility.should == "fedoraAdmin"
-    record.date.should == "2008-11-19T18:18:49.003Z"
+    record.date.should == "2013-02-25T16:43:06.219Z"
     record.justification.should == ""
   end
   
