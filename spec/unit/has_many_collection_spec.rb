9c3087bb5c101512f8aabf853e4c403e9962cbd8
Chris Beer <cabeer@stanford.edu>
2013-04-02 14:49:50 -0700

    Fix has and belogns to many, so it calls remove_relationship on the right object.

--- spec/unit/has_many_collection_spec.rb
+++ spec/unit/has_many_collection_spec.rb
@@ -1,6 +1,32 @@
 require 'spec_helper'
 
 describe ActiveFedora::Associations::HasManyAssociation do
+  it "should call add_relationship" do
+    subject = stub("subject", :new_record? => false, :pid => 'subject:a', :internal_uri => 'info:fedora/subject:a')
+    predicate = stub(:klass => mock.class, :options=>{:property=>'predicate'}, :class_name=> nil)
+    ActiveFedora::SolrService.stub(:query).and_return([])
+    ac = ActiveFedora::Associations::HasManyAssociation.new(subject, predicate)
+    object = stub("object", :new_record? => false, :pid => 'object:b', :save => nil)
+  
+    object.should_receive(:add_relationship).with('predicate', subject)
+ 
+    ac << object
+
+  end
+  
+  it "should call remove_relationship" do
+    subject = stub("subject", :new_record? => false, :pid => 'subject:a', :internal_uri => 'info:fedora/subject:a')
+    predicate = stub(:klass => mock.class, :options=>{:property=>'predicate'}, :class_name=> nil)
+    ActiveFedora::SolrService.stub(:query).and_return([])
+    ac = ActiveFedora::Associations::HasManyAssociation.new(subject, predicate)
+    object = stub("object", :new_record? => false, :pid => 'object:b', :save => nil)
+  
+    object.should_receive(:remove_relationship).with('predicate', subject)
+ 
+    ac.delete(object)
+
+  end
+
   it "should be able to replace the collection" do
     @owner = stub(:new_record? => false, :to_ary => nil, :internal_uri => 'info:fedora/changeme:99')
     @reflection = stub(:klass => mock.class, :options=>{:property=>'predicate'}, :class_name=> nil)
