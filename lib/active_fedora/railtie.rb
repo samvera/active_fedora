9e78a1c749ff340114f12023a202ed56bbfdcda4
Justin Coyne <justin@curationexperts.com>
2013-07-01 13:31:02 -0500

    Autoload classes in app/models/datastreams

--- lib/active_fedora/railtie.rb
+++ lib/active_fedora/railtie.rb
@@ -3,6 +3,11 @@ module ActiveFedora
     rake_tasks do
       load "tasks/active_fedora.rake"
     end
+
+   initializer 'activefedora.autoload', :before => :set_autoload_paths do |app|
+     app.config.autoload_paths << 'app/models/datastreams'
+    end
+
     generators do
       require(
         'generators/active_fedora/config/config_generator'
