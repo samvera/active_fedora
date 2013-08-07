7854679126d3be423ba2af5d0b33ad499a73d705
Justin Coyne <justin@curationexperts.com>
2013-08-07 11:39:16 -0500

    Raise an error when trying to assign to a delegated attribute that hasn't been delegated

--- spec/unit/base_delegate_spec.rb
+++ spec/unit/base_delegate_spec.rb
@@ -58,9 +58,7 @@ describe ActiveFedora::Base do
       Object.send(:remove_const, :BarHistory2)
     end
 
-    before :each do
-      @n = BarHistory2.new()
-    end
+    subject { BarHistory2.new() }
 
     it "should reveal the unique properties" do
       BarHistory2.unique?(:fubar).should be_true
@@ -68,40 +66,48 @@ describe ActiveFedora::Base do
     end
 
     it "should save a delegated property uniquely" do
-      @n.fubar="Quack"
-      @n.fubar.should == "Quack"
-      @n.withText.get_values(:fubar).first.should == 'Quack'
-      @n.donkey="Bray"
-      @n.donkey.should == "Bray"
-      @n.xmlish.term_values(:donkey).first.should == 'Bray'
+      subject.fubar="Quack"
+      subject.fubar.should == "Quack"
+      subject.withText.get_values(:fubar).first.should == 'Quack'
+      subject.donkey="Bray"
+      subject.donkey.should == "Bray"
+      subject.xmlish.term_values(:donkey).first.should == 'Bray'
     end
 
     it "should allow passing parameters to the delegate accessor" do
-      @n.cow=["one", "two"]
-      @n.cow(1).should == ['two']
+      subject.cow=["one", "two"]
+      subject.cow(1).should == ['two']
     end
 
 
     it "should return an array if not marked as unique" do
       ### Metadata datastream does not appear to support multiple value setting
-      @n.cow=["one", "two"]
-      @n.cow.should == ["one", "two"]
+      subject.cow=["one", "two"]
+      subject.cow.should == ["one", "two"]
     end
 
     it "should be able to delegate deeply into the terminology" do
-      @n.duck=["Quack", "Peep"]
-      @n.duck.should == ["Quack", "Peep"]
+      subject.duck=["Quack", "Peep"]
+      subject.duck.should == ["Quack", "Peep"]
     end
 
     describe "array getters and setters" do
       it "should accept symbol keys" do
-        @n[:duck]= ["Cluck", "Gobble"]
-        @n[:duck].should == ["Cluck", "Gobble"]
+        subject[:duck]= ["Cluck", "Gobble"]
+        subject[:duck].should == ["Cluck", "Gobble"]
       end
 
       it "should accept string keys" do
-        @n['duck']= ["Cluck", "Gobble"]
-        @n['duck'].should == ["Cluck", "Gobble"]
+        subject['duck']= ["Cluck", "Gobble"]
+        subject['duck'].should == ["Cluck", "Gobble"]
+      end
+
+      it "should raise an error on the reader when the field isn't delegated" do
+        expect {subject['goose'] }.to raise_error ActiveFedora::UnknownAttributeError, "BarHistory2 does not have an attribute `goose'"
+      end
+
+      it "should raise an error on the setter when the field isn't delegated" do
+        expect {subject['goose']="honk" }.to raise_error ActiveFedora::UnknownAttributeError, "BarHistory2 does not have an attribute `goose'"
       end
     end
 
