60425b778bcca409902d4d09077bdd5353ea1a3b
Justin Coyne <justin@curationexperts.com>
2013-11-27 12:19:40 -0600

    Move uri methods to SemanticNode

--- lib/active_fedora/base.rb
+++ lib/active_fedora/base.rb
@@ -12,7 +12,7 @@ module ActiveFedora
   #
   # =The Basics
   #   class Oralhistory < ActiveFedora::Base
-  #     has_metadata :name => "properties", :type => ActiveFedora::SimpleDatastream do |m|
+  #     has_metadata "properties", type: ActiveFedora::SimpleDatastream do |m|
   #       m.field "narrator",  :string
   #       m.field "narrator",  :text
   #     end
@@ -25,43 +25,6 @@ module ActiveFedora
   #
   class Base
     include SemanticNode
-    extend Deprecation
-
-    #return the internal fedora URI
-    def internal_uri
-      self.class.internal_uri(pid)
-    end
-
-    def self.internal_uri(pid)
-      "info:fedora/#{pid}"
-    end
-
-    # @param [String,Array] uris a single uri (as a string) or a list of uris to convert to pids
-    # @returns [String] the pid component of the URI
-    def self.pids_from_uris(uris) 
-      Deprecation.warn(Base, "pids_from_uris has been deprecated and will be removed in active-fedora 8.0.0", caller)
-      if uris.kind_of? String
-        pid_from_uri(uris)
-      else
-        Array(uris).map {|uri| pid_from_uri(uri)}
-      end
-    end
-
-    # Returns a suitable uri object for :has_model
-    # Should reverse Model#from_class_uri
-    def self.to_class_uri(attrs = {})
-      if self.respond_to? :pid_suffix
-        pid_suffix = self.pid_suffix
-      else
-        pid_suffix = attrs.fetch(:pid_suffix, ContentModel::CMODEL_PID_SUFFIX)
-      end
-      if self.respond_to? :pid_namespace
-        namespace = self.pid_namespace
-      else
-        namespace = attrs.fetch(:namespace, ContentModel::CMODEL_NAMESPACE)
-      end
-      "info:fedora/#{namespace}:#{ContentModel.sanitized_class_name(self)}#{pid_suffix}" 
-    end
   end
 
   Base.class_eval do
