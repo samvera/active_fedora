89f5522c7ed9b16115c975d9a9780e8ce6160d60
Justin Coyne <justin@curationexperts.com>
2014-01-17 20:53:14 -0600

    When an object is loaded from solr, it should be able to access attributes in an om datastream

--- spec/integration/load_from_solr_spec.rb
+++ spec/integration/load_from_solr_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "Loading from solr" do
   before do
-    class MyDatastream < ActiveFedora::NtriplesRDFDatastream
+    class MyRdfDatastream < ActiveFedora::NtriplesRDFDatastream
       map_predicates do |map|
         map.title(in: RDF::DC) do |index|
           index.as :stored_searchable, :facetable
@@ -20,21 +20,31 @@ describe "Loading from solr" do
         map.related_url(to: "seeAlso", in: RDF::RDFS)
       end
     end
+    class MyOmDatastream < ActiveFedora::OmDatastream
+      set_terminology do |t|
+        t.root(path: "animals")
+        t.duck(index_as: :stored_searchable)
+      end
+    end
     class RdfTest < ActiveFedora::Base 
-      has_metadata :name=>'rdf', :type=>MyDatastream
+      has_metadata 'rdf', type: MyRdfDatastream
+      has_metadata 'om', type: MyOmDatastream
       has_attributes :based_near, :related_url, :part, :date_uploaded, datastream: 'rdf', multiple: true
       has_attributes :title, :identifier, datastream: 'rdf', multiple: false
+      has_attributes :duck, datastream: 'om', multiple: false
     end
   end
 
   let!(:original) { RdfTest.create!(title: "PLAN 9 FROM OUTER SPACE",
                                     date_uploaded: Date.parse('1959-01-01'),
+                                    duck: "quack",
                                    identifier: 12345) }
 
   after do
     original.destroy
     Object.send(:remove_const, :RdfTest)
-    Object.send(:remove_const, :MyDatastream)
+    Object.send(:remove_const, :MyRdfDatastream)
+    Object.send(:remove_const, :MyOmDatastream)
   end
 
   it "should be able to get indexed properties without loading from fedora" do
@@ -44,5 +54,8 @@ describe "Loading from solr" do
     expect(obj.date_uploaded).to eq [Date.parse('1959-01-01')]
     expect(obj.identifier).to eq 12345
     expect{obj.part}.to raise_error KeyError, "Tried to fetch `part' from solr, but it isn't indexed."
+    expect(ActiveFedora::DatastreamAttribute.logger).to receive(:info).with "Couldn't get duck out of solr, because the datastream 'MyOmDatastream' doesn't respond to 'primary_solr_name'. Trying another way."
+    expect(obj.duck).to eq 'quack'
   end
+
 end
