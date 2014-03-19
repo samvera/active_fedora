907652b72b4a0ccc2c89859ddac536fa13100bf4
Justin Coyne <justin@curationexperts.com>
2014-03-19 09:44:25 -0500

    Removed deprecated method quote_for_solr

--- lib/active_fedora/querying.rb
+++ lib/active_fedora/querying.rb
@@ -1,8 +1,5 @@
 module ActiveFedora
   module Querying
-    extend Deprecation
-    self.deprecation_horizon = 'active-fedora version 8.0.0'
-
     delegate :find, :first, :exists?, :where, :limit, :order, :delete_all, 
       :destroy_all, :count, :last, :find_with_conditions, :find_in_batches, :find_each, :to=>:all
 
@@ -15,9 +12,5 @@ module ActiveFedora
       [ActiveFedora::SolrService.solr_name(:system_create, :stored_sortable, type: :date)+' asc'] 
     end
 
-    def quote_for_solr(value)
-      RSolr.escape(value)
-    end
-    deprecation_deprecate :quote_for_solr
   end
 end
