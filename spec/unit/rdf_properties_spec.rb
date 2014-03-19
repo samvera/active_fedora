a3077fb4e59ec3bedced18369c73c9182274c9e2
Justin Coyne <justin@curationexperts.com>
2014-03-19 09:56:43 -0500

    Remove deprecated method map_properties

--- spec/unit/rdf_properties_spec.rb
+++ spec/unit/rdf_properties_spec.rb
@@ -10,28 +10,6 @@ describe ActiveFedora::Rdf::Properties do
     Object.send(:remove_const, "DummyProperties")
   end
 
-  describe '#map_predicates' do
-    before do
-      DummyProperties.map_predicates do |map|
-        map.title(:in => RDF::DC, :class_name => RDF::Literal) do |index|
-          index.as :facetable, :searchable
-        end
-      end
-    end
-
-    it 'should set a property' do
-      expect(DummyProperties.properties).to include :title
-    end
-
-    it "should set index behaviors" do
-      expect(DummyProperties.properties[:title][:behaviors]).to eq [:facetable, :searchable]
-    end
-
-    it "should set a class name" do
-      expect(DummyProperties.properties[:title][:class_name]).to eq RDF::Literal
-    end
-  end
-
   describe '#property' do
     it 'should set a property' do
       DummyProperties.property :title, :predicate => RDF::DC.title
