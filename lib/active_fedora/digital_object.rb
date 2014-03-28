98f2c1831febf070ce66f432874540c05a993e35
Justin Coyne <justin@curationexperts.com>
2014-03-27 20:46:19 -0400

    Save datastreams after save

--- lib/active_fedora/digital_object.rb
+++ lib/active_fedora/digital_object.rb
@@ -7,23 +7,9 @@ module ActiveFedora
         # ds_spec is nil when called from Rubydora for existing datastreams, so it should not be autocreated
         ds_spec ||= (original_class.ds_specs[dsid] || {}).merge(:autocreate=>false)
         ds = ds_spec.fetch(:type, ActiveFedora::Datastream).new(self, dsid, options)
-        attributes = {}
-        attributes[:asOfDateTime] ||= asOfDateTime if self.respond_to? :asOfDateTime
-        attributes[:dsLabel] = ds_spec[:label] if ds_spec[:label].present?
-        attributes[:controlGroup] = ds_spec[:control_group] if ds_spec[:control_group].present?
-        attributes[:versionable] = ds_spec[:versionable] unless ds_spec[:versionable].nil?
-        if attributes[:controlGroup]=='E'
-          if !ds_spec[:disseminator].present? && ds_spec[:url].present?
-            attributes[:dsLocation]= ds_spec[:url]
-          end
-        elsif attributes[:controlGroup]=='R'
-          attributes[:dsLocation]= ds_spec[:url]
-        end
-        ds.default_attributes = attributes
-        # would rather not load profile here in the call to .new?, so trusting :autocreate attribute
-        if ds_spec[:autocreate] # and ds.new?
-          # TODO need to do something here
-          #ds.datastream_will_change!
+        ds.default_attributes = {}
+        if ds_spec[:autocreate]
+          ds.datastream_will_change!
         end
         ds
       end
--- lib/active_fedora/persistence.rb
+++ lib/active_fedora/persistence.rb
@@ -162,6 +162,10 @@ module ActiveFedora
       # @inner_object.datastreams.each do |dsid, ds|
       #   datastreams[dsid] = ds
       # end 
+      datastreams.select { |_, ds| ds.changed? }.each do |_, ds|
+        ds.save
+      end
+
       refresh
       update_index if should_update_index
     end
