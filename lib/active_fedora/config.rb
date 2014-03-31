29a4da3556621d7a7ca50a330e28f26082fd6f66
Justin Coyne <justin@curationexperts.com>
2014-03-31 10:43:20 -0500

    remove sharding

--- lib/active_fedora/config.rb
+++ lib/active_fedora/config.rb
@@ -2,28 +2,10 @@ module ActiveFedora
   class Config
     attr_reader :credentials
     def initialize(val)
-      if val.is_a? Array
-        init_shards(val)
-      else 
-        init_single(val)
+      @credentials = val.symbolize_keys
+      unless @credentials.has_key?(:url)
+        raise ActiveFedora::ConfigurationError, "Fedora configuration must provide :url."
       end
-    end
-
-    def sharded?
-      credentials.is_a? Array
-    end
-
-    private
-
-    def init_shards(vals)
-        @credentials = vals.map(&:symbolize_keys)
-    end
-
-    def init_single(vals)
-        @credentials = vals.symbolize_keys
-        unless @credentials.has_key?(:user) && @credentials.has_key?(:password) && @credentials.has_key?(:url)
-          raise ActiveFedora::ConfigurationError, "Fedora configuration must provide :user, :password and :url."
-        end
     end
   end
 end
