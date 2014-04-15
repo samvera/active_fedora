16a8c2ba159e7e7f9453ea0bff9dc39ae27e0477
Justin Coyne <justin@curationexperts.com>
2014-04-14 23:21:40 -0500

    Dates should be single values

--- lib/active_fedora/fedora_attributes.rb
+++ lib/active_fedora/fedora_attributes.rb
@@ -5,8 +5,8 @@ module ActiveFedora
     included do
       attribute :has_model, [ RDF::URI.new("info:fedora/fedora-system:def/relations-external#hasModel")]
       # TODO is it possible to put defaults here?
-      attribute :create_date, [ RDF::URI.new("http://fedora.info/definitions/v4/repository#created")]
-      attribute :modified_date, [ RDF::URI.new("http://fedora.info/definitions/v4/repository#lastModified")]
+      attribute :create_date, [ RDF::URI.new("http://fedora.info/definitions/v4/repository#created"), FedoraLens::Lenses.single, FedoraLens::Lenses.literal_to_string ]
+      attribute :modified_date, [ RDF::URI.new("http://fedora.info/definitions/v4/repository#lastModified"), FedoraLens::Lenses.single, FedoraLens::Lenses.literal_to_string ]
     end
 
 
