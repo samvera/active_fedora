ad872b769c58d107e76f5845aecd70ce1602e536
Justin Coyne <justin@curationexperts.com>
2014-04-22 13:54:20 -0700

    Load datastreams that are in the repository, but not declared

--- lib/active_fedora/fedora_attributes.rb
+++ lib/active_fedora/fedora_attributes.rb
@@ -4,6 +4,8 @@ module ActiveFedora
 
     included do
       attribute :has_model, [ RDF::URI.new("http://fedora.info/definitions/v4/rels-ext#hasModel"), FedoraLens::Lenses.single, FedoraLens::Lenses.literal_to_string ]
+      attribute :datastream_assertions, [ RDF::URI.new("http://fedora.info/definitions/v4/repository#hasChild") ]
+
       # TODO is it possible to put defaults here?
       attribute :create_date, [ RDF::URI.new("http://fedora.info/definitions/v4/repository#created"), FedoraLens::Lenses.single, FedoraLens::Lenses.literal_to_string ]
       attribute :modified_date, [ RDF::URI.new("http://fedora.info/definitions/v4/repository#lastModified"), FedoraLens::Lenses.single, FedoraLens::Lenses.literal_to_string ]
