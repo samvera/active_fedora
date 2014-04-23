267ec1d8d4ed6055b9f40fd511a1c5701e879e99
Justin Coyne <justin@curationexperts.com>
2014-04-23 13:37:32 -0700

    Strip leading slash from to_key

--- lib/active_fedora/base.rb
+++ lib/active_fedora/base.rb
@@ -49,5 +49,10 @@ module ActiveFedora
 
     puts "Creds #{ActiveFedora.config.credentials.inspect}"
 
+    # If the id is "/foo:1" then to_key ought to return ["foo:1"]
+    def to_key
+      id && [id.sub('/', '')]
+    end
+
   end
 end
