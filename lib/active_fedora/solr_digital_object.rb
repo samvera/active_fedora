e18a38bc389d20b9ee756e9f6e2734af2c6d2929
Justin Coyne <justin@curationexperts.com>
2014-04-28 13:00:05 -0500

    Fixed initializing datastreams from solr_digital_object

--- lib/active_fedora/solr_digital_object.rb
+++ lib/active_fedora/solr_digital_object.rb
@@ -21,7 +21,7 @@ module ActiveFedora
         #Initialize the datastreams that are in the solr document, but not found in the classes spec.
         mime_type = profile_hash['datastreams'][dsid]['dsMIME']
         ds_class = mime_type =~ /[\/\+]xml$/ ? OmDatastream : Datastream
-        @datastreams[dsid] = ds_class.new(self, dsid)
+        @datastreams[dsid] = ds_class.new(klass.new(@pid), dsid)
       end
 
       @label = @profile['objLabel']
