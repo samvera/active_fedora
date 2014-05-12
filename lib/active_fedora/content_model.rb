97799afa5d88a97c3c21cc5bbecb5276e2a0ba33
Justin Coyne <justin@curationexperts.com>
2014-05-12 14:01:58 -0500

    Remove invalid call to construct_query_for_pids. Reduced total number of Solr queries. Fixes #418

--- lib/active_fedora/content_model.rb
+++ lib/active_fedora/content_model.rb
@@ -1,15 +1,7 @@
 module ActiveFedora
-  class ContentModel < Base
+  module ContentModel
     CMODEL_NAMESPACE = "afmodel"
     CMODEL_PID_SUFFIX = ""
-    
-    attr_accessor :pid_suffix, :namespace
-    
-    def initialize(attrs={})
-      @pid_suffix = attrs.has_key?(:pid_suffix) ? attrs[:pid_suffix] : CMODEL_PID_SUFFIX
-      @namespace = attrs.has_key?(:namespace) ? attrs[:namespace] : CMODEL_NAMESPACE
-      super
-    end
     
     ###Override this, if you prefer your class names serialized some other way
     def self.sanitized_class_name(klass)
