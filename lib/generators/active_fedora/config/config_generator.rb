30f2daf5ade48dd721e3ace31d65fa1c78783c3d
Justin Coyne <justin@curationexperts.com>
2014-07-22 20:20:24 -0500

    remove the fedora generator

--- lib/generators/active_fedora/config/config_generator.rb
+++ lib/generators/active_fedora/config/config_generator.rb
@@ -4,7 +4,6 @@ module ActiveFedora
   class ConfigGenerator < Rails::Generators::Base
     def generate_configs
       generate('active_fedora:config:solr')
-      generate('active_fedora:config:fedora')
     end
   end
 end
