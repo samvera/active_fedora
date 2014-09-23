7c98f53397c43ffe9694ae981346e70ffd616159
dchandekstark <dchandekstark@gmail.com>
2014-09-22 23:10:55 -0400

    Factors out behavior from ActiveFedora::Rdf::ObjectResource to mixin ActiveFedora::Rdf::Persistence.
    The change allows any ActiveTriples::Resource descendant which includes the mixin
    to be used as the resource class for an ActiveFedora::RDFDatastream, rather than
    requiring an ObjectResource subclass.
    
    Conflicts:
    	lib/active_fedora/rdf.rb
    	lib/active_fedora/rdf/object_resource.rb

--- lib/active_fedora/rdf.rb
+++ lib/active_fedora/rdf.rb
@@ -6,5 +6,17 @@ module ActiveFedora
     autoload :Indexing
     autoload :Identifiable
     autoload :ObjectResource
+    autoload :Persistence
+
+    # Aliases for deprecated ActiveFedora::Rdf Classes/Modules
+    # TODO: Remove in 8.0.0
+    Resource = ActiveSupport::Deprecation::DeprecatedConstantProxy.new('ActiveFedora::Rdf::Resource', 'ActiveTriples::Resource')
+    Term = ActiveSupport::Deprecation::DeprecatedConstantProxy.new('ActiveFedora::Rdf::Term', 'ActiveTriples::Term')
+    List = ActiveSupport::Deprecation::DeprecatedConstantProxy.new('ActiveFedora::Rdf::List', 'ActiveTriples::List')
+    Configurable = ActiveSupport::Deprecation::DeprecatedConstantProxy.new('ActiveFedora::Rdf::Configurable', 'ActiveTriples::Configurable')
+    Properties = ActiveSupport::Deprecation::DeprecatedConstantProxy.new('ActiveFedora::Rdf::Properties', 'ActiveTriples::Properties')
+    Repositories = ActiveSupport::Deprecation::DeprecatedConstantProxy.new('ActiveFedora::Rdf::Repositories', 'ActiveTriples::Repositories')
+    NodeConfig = ActiveSupport::Deprecation::DeprecatedConstantProxy.new('ActiveFedora::Rdf::NodeConfig', 'ActiveTriples::NodeConfig')
+    NestedAttributes = ActiveSupport::Deprecation::DeprecatedConstantProxy.new('ActiveFedora::Rdf::NestedAttributes', 'ActiveTriples::NestedAttributes')
   end
 end
