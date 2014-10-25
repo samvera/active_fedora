1bdf5084c3435e98361471a55e584c649d810c1e
Justin Coyne <justin@curationexperts.com>
2014-10-25 13:20:15 -0400

    For testing, use a URI on our server. Fix rspec deprecations.

--- spec/unit/simple_datastream_spec.rb
+++ spec/unit/simple_datastream_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 describe ActiveFedora::SimpleDatastream do
 
-  let(:digital_object) { double(new_record?: false, uri: 'http://example.com/rest/test/foo123') }
+  let(:digital_object) { double(new_record?: false, uri: "#{ActiveFedora.fedora.host}#{ActiveFedora.fedora.base_path}/foo123") }
   let(:sample_xml) { "<fields><coverage>coverage1</coverage><coverage>coverage2</coverage><creation_date>2012-01-15</creation_date><mydate>fake-date</mydate><publisher>publisher1</publisher></fields>" }
 
   before do
     @test_ds = ActiveFedora::SimpleDatastream.new(digital_object, 'test_ds')
-    @test_ds.stub(retrieve_content: '') # DS grabs the old content to compare against the new
+    allow(@test_ds).to receive(:retrieve_content).and_return('') # DS grabs the old content to compare against the new
     @test_ds.content = sample_xml
     @test_ds.field :coverage
     @test_ds.field :creation_date, :date
@@ -17,35 +17,35 @@ describe ActiveFedora::SimpleDatastream do
   end
 
   it "ng_xml should parse everything correctly" do
-    @test_ds.ng_xml.should be_equivalent_to sample_xml
+    expect(@test_ds.ng_xml).to be_equivalent_to sample_xml
   end
 
-  
+
   describe '#new' do
-    describe "model methods" do 
+    describe "model methods" do
 
       [:coverage, :mydate, :publisher].each do |el|
         it "should respond to getters and setters for the string typed #{el} element" do
           value = "Hey #{el}"
-          @test_ds.send("#{el.to_s}=", value) 
-          @test_ds.send(el).first.should == value  #Looking at first because creator has 2 nodes
+          @test_ds.send("#{el.to_s}=", value)
+          expect(@test_ds.send(el).first).to eq value  #Looking at first because creator has 2 nodes
         end
       end
 
       it "should set date elements" do
         d = Date.parse('1939-05-23')
         @test_ds.creation_date = d
-        @test_ds.creation_date.first.should == d
+        expect(@test_ds.creation_date.first).to eq d
       end
     end
   end
-  
+
   describe '.to_xml' do
     it 'should output the fields hash as Qualified Dublin Core XML' do
       @test_ds.publisher= "charlie"
       @test_ds.coverage= ["80%", "20%"]
 
-      @test_ds.to_xml.should be_equivalent_to('
+      expect(@test_ds.to_xml).to be_equivalent_to('
         <fields>
           <coverage>80%</coverage>
           <coverage>20%</coverage>
@@ -59,8 +59,8 @@ describe ActiveFedora::SimpleDatastream do
   describe "#to_solr" do
     it "should have title" do
       solr = @test_ds.to_solr
-      solr[ActiveFedora::SolrService.solr_name('publisher', type: :string)].should == "publisher1"
-      solr[ActiveFedora::SolrService.solr_name('creation_date', type: :date)].should == "2012-01-15"
+      expect(solr[ActiveFedora::SolrService.solr_name('publisher', type: :string)]).to eq "publisher1"
+      expect(solr[ActiveFedora::SolrService.solr_name('creation_date', type: :date)]).to eq "2012-01-15"
     end
   end
 
