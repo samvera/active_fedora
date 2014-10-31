2a1260097cc1424cd65b00a22d04bb6cff8ae83f
Justin Coyne <justin@curationexperts.com>
2014-10-30 20:35:51 -0500

    Use the namespaced (root) File model in solr config generator

--- lib/generators/active_fedora/config/solr/solr_generator.rb
+++ lib/generators/active_fedora/config/solr/solr_generator.rb
@@ -2,7 +2,7 @@ require 'rails/generators'
 
 module ActiveFedora
   class Config::SolrGenerator < Rails::Generators::Base
-    source_root File.expand_path('../templates', __FILE__)
+    source_root ::File.expand_path('../templates', __FILE__)
 
     def generate
       # Overwrite the configuration files that Blacklight has installed
