ee615bdc99e9cc33c00b0cf3a55f01377020c9c0
Justin Coyne <justin@curationexperts.com>
2014-11-13 14:04:50 -0600

    Removed a hash rocket

--- spec/unit/files_hash_spec.rb
+++ spec/unit/files_hash_spec.rb
@@ -4,7 +4,7 @@ describe ActiveFedora::FilesHash do
   before do
     class Container
     end
-    allow(Container).to receive(:child_resource_reflections).and_return({:file => reflection })
+    allow(Container).to receive(:child_resource_reflections).and_return(file: reflection)
     allow(container).to receive(:association).with(:file).and_return(association)
     allow(container).to receive(:undeclared_files).and_return([])
   end
