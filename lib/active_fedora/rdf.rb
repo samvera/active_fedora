20fceb7b9700bdde10fcc8e9e9a04fe4a5d5c6c6
dchandekstark <dchandekstark@gmail.com>
2014-12-02 16:41:29 -0500

    Removed pending deprecations omitted in 8.0.0.rc1 Bumped version to 8.0.0.rc2

--- lib/active_fedora/rdf.rb
+++ lib/active_fedora/rdf.rb
@@ -7,17 +7,5 @@ module ActiveFedora
     autoload :ObjectResource
     autoload :Persistence
     autoload :ProjectHydra
-
-    # Aliases for deprecated ActiveFedora::RDF Classes/Modules
-    # TODO: Remove in 8.0.0
-    Resource = ActiveSupport::Deprecation::DeprecatedConstantProxy.new('ActiveFedora::Rdf::Resource', 'ActiveTriples::Resource')
-    Term = ActiveSupport::Deprecation::DeprecatedConstantProxy.new('ActiveFedora::Rdf::Term', 'ActiveTriples::Term')
-    List = ActiveSupport::Deprecation::DeprecatedConstantProxy.new('ActiveFedora::Rdf::List', 'ActiveTriples::List')
-    Configurable = ActiveSupport::Deprecation::DeprecatedConstantProxy.new('ActiveFedora::Rdf::Configurable', 'ActiveTriples::Configurable')
-    Properties = ActiveSupport::Deprecation::DeprecatedConstantProxy.new('ActiveFedora::Rdf::Properties', 'ActiveTriples::Properties')
-    Repositories = ActiveSupport::Deprecation::DeprecatedConstantProxy.new('ActiveFedora::Rdf::Repositories', 'ActiveTriples::Repositories')
-    NodeConfig = ActiveSupport::Deprecation::DeprecatedConstantProxy.new('ActiveFedora::Rdf::NodeConfig', 'ActiveTriples::NodeConfig')
-    NestedAttributes = ActiveSupport::Deprecation::DeprecatedConstantProxy.new('ActiveFedora::Rdf::NestedAttributes', 'ActiveTriples::NestedAttributes')
   end
-  Rdf = ActiveSupport::Deprecation::DeprecatedConstantProxy.new('ActiveFedora::Rdf', 'ActiveFedora::RDF')
 end
