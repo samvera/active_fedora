9429fd8e001ba65f4f032f80b21176bd9c7517f2
Justin Coyne <justin@curationexperts.com>
2014-12-05 15:21:38 -0600

    Init the base path when the Fedora object is initialized

--- lib/active_fedora.rb
+++ lib/active_fedora.rb
@@ -158,7 +158,6 @@ module ActiveFedora #:nodoc:
       SolrService.reset!
       Predicates.predicate_config = nil
       configurator.init(options)
-      fedora.init_base_path
     end
 
     def config
