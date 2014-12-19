fdcd520a9e18965a86293823575691d091608eef
Joe Atzberger <ohiocore@gmail.com>
2014-12-19 15:45:29 -0800

    Modernize rspec syntax for model_spec

--- spec/unit/model_spec.rb
+++ spec/unit/model_spec.rb
@@ -1,74 +1,71 @@
 require 'spec_helper'
 
-describe ActiveFedora::Model do
-  
-  before(:all) do
-    module SpecModel
-      class Basic < ActiveFedora::Base
-      end
-    end
-    @model_query = "has_model_s:#{solr_uri("info:fedora/afmodel:SpecModel_Basic")}"
+class SpecModelBasic < ActiveFedora::Base
+end
+module SpecModel
+  class CamelCased
+    include ActiveFedora::Model
   end
+end
+
+describe ActiveFedora::Model do
   
   after(:all) do
     Object.send(:remove_const, :SpecModel)
   end
   
+  before :each do
+    @cc = SpecModel::CamelCased.new
+  end
+
   describe '.solr_query_handler' do
     after do
       # reset to default
-      SpecModel::Basic.solr_query_handler = 'standard'
+      SpecModelBasic.solr_query_handler = 'standard'
     end
     it "should have a default" do
-      SpecModel::Basic.solr_query_handler.should == 'standard'
+      SpecModelBasic.solr_query_handler.should == 'standard'
     end
     it "should be settable" do
-      SpecModel::Basic.solr_query_handler = 'search'
-      SpecModel::Basic.solr_query_handler.should == 'search'
+      SpecModelBasic.solr_query_handler = 'search'
+      SpecModelBasic.solr_query_handler.should == 'search'
     end
   end
-  
-  describe "URI translation" do
-    before :all do
-      module SpecModel
-        class CamelCased
-          include ActiveFedora::Model
-        end
-      end
 
-    end
-    
-    after :all do
-      SpecModel.send(:remove_const, :CamelCased)
-    end
-    subject {SpecModel::CamelCased}
-    
-    its(:to_class_uri) {should == 'info:fedora/afmodel:SpecModel_CamelCased' }
-  
-    context "with the namespace declared in the model" do
-      before do
-        subject.stub(:pid_namespace).and_return("test-cModel")
-      end
-      its(:to_class_uri) {should == 'info:fedora/test-cModel:SpecModel_CamelCased' }
-    end
-    context "with the suffix declared in the model" do
-      before do
-        subject.stub(:pid_suffix).and_return("-TEST-SUFFIX")
-      end
-      its(:to_class_uri) {should == 'info:fedora/afmodel:SpecModel_CamelCased-TEST-SUFFIX' }
-    end
-  
-    describe ".classname_from_uri" do 
-      it "should turn an afmodel URI into a Model class name" do
-        ActiveFedora::Model.classname_from_uri('info:fedora/afmodel:SpecModel_CamelCased').should == ['SpecModel::CamelCased', 'afmodel']
-      end
-      it "should not change plurality" do
-        ActiveFedora::Model.classname_from_uri('info:fedora/afmodel:MyMetadata').should == ['MyMetadata', 'afmodel']
-      end
-      it "should capitalize the first letter" do
-        ActiveFedora::Model.classname_from_uri('info:fedora/afmodel:image').should == ['Image', 'afmodel']
-      end
+  describe "URI translation" do
+    it "to_class_uri correct" do
+      expect(SpecModel::CamelCased.to_class_uri).to eq 'info:fedora/afmodel:SpecModel_CamelCased'
     end
   end
-  
+
+  describe "URI translation" do
+    before :each do 
+      allow(SpecModel::CamelCased).to receive(:pid_namespace).and_return("test-cModel")
+    end
+    it "with the namespace declared in the model" do
+      expect(SpecModel::CamelCased.to_class_uri).to eq 'info:fedora/test-cModel:SpecModel_CamelCased'
+    end
+  end
+
+  describe "URI translation" do
+    before :each do
+      allow(SpecModel::CamelCased).to receive(:pid_suffix).and_return("-TEST-SUFFIX")
+    end
+    it "with the suffix declared in the model" do
+      expect(SpecModel::CamelCased.to_class_uri).to eq 'info:fedora/afmodel:SpecModel_CamelCased-TEST-SUFFIX'
+    end
+  end
+
+  describe ".classname_from_uri" do
+    it "should turn an afmodel URI into a Model class name" do
+      ActiveFedora::Model.classname_from_uri('info:fedora/afmodel:SpecModel_CamelCased').should == ['SpecModel::CamelCased', 'afmodel']
+    end
+    it "should not change plurality" do
+      ActiveFedora::Model.classname_from_uri('info:fedora/afmodel:MyMetadata').should == ['MyMetadata', 'afmodel']
+    end
+    it "should capitalize the first letter" do
+      ActiveFedora::Model.classname_from_uri('info:fedora/afmodel:image').should == ['Image', 'afmodel']
+    end
+  end
+
 end
