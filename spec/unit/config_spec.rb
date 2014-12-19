077a25e29e9a61459d530b793d9eb00d5dd624c0
Joe Atzberger <ohiocore@gmail.com>
2014-12-19 14:34:08 -0800

    Modernize rspec syntax

--- spec/unit/config_spec.rb
+++ spec/unit/config_spec.rb
@@ -2,18 +2,30 @@ require 'spec_helper'
 
 describe ActiveFedora::Config do
   describe "with a single fedora instance" do
-    conf = YAML.load(File.read('spec/fixtures/rails_root/config/fedora.yml'))['test']
-    subject { ActiveFedora::Config.new(conf) }
-    its(:credentials) { should == {:url => 'http://testhost.com:8983/fedora', :user=> 'fedoraAdmin', :password=> 'fedoraAdmin'}}
-    it { should_not be_sharded }
+    before(:each) do
+        yaml = YAML.load(File.read('spec/fixtures/rails_root/config/fedora.yml'))['test']
+        @conf = ActiveFedora::Config.new(yaml)
+    end
+    it '#credentials' do
+      expect(@conf.credentials).to eq({:url => 'http://testhost.com:8983/fedora', :user=> 'fedoraAdmin', :password=> 'fedoraAdmin'})
+      expect(@conf).not_to be_sharded
+    end
   end
+
   describe "with several fedora shards" do
-    conf = YAML.load(File.read('spec/fixtures/sharded_fedora.yml'))['test']
-    subject { ActiveFedora::Config.new(conf) }
-    its(:credentials) { should == [{:url => 'http://127.0.0.1:8983/fedora1', :user=> 'fedoraAdmin', :password=> 'fedoraAdmin'},
-                              {:url => 'http://127.0.0.1:8983/fedora2', :user=> 'fedoraAdmin', :password=> 'fedoraAdmin'},
-                              {:url => 'http://127.0.0.1:8983/fedora3', :user=> 'fedoraAdmin', :password=> 'fedoraAdmin'}]}
-    it { should be_sharded }
+    before(:each) do
+        yaml = YAML.load(File.read('spec/fixtures/sharded_fedora.yml'))['test']
+        @conf = ActiveFedora::Config.new(yaml)
+    end
+
+    it '#credentials' do
+      expect(@conf.credentials).to eq([
+        {:url => 'http://127.0.0.1:8983/fedora1', :user=> 'fedoraAdmin', :password=> 'fedoraAdmin'},
+        {:url => 'http://127.0.0.1:8983/fedora2', :user=> 'fedoraAdmin', :password=> 'fedoraAdmin'},
+        {:url => 'http://127.0.0.1:8983/fedora3', :user=> 'fedoraAdmin', :password=> 'fedoraAdmin'}
+      ])
+      expect(@conf).to be_sharded
+    end
   end
 
 end
