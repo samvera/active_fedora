6b41fcda53c88210a98657bcac1101124902e1af
Justin Coyne <justin@curationexperts.com>
2014-12-22 07:52:16 -0600

    Restore the generator for fedora.yml

--- lib/generators/active_fedora/config/config_generator.rb
+++ lib/generators/active_fedora/config/config_generator.rb
@@ -3,6 +3,7 @@ require 'rails/generators'
 module ActiveFedora
   class ConfigGenerator < Rails::Generators::Base
     def generate_configs
+      generate('active_fedora:config:fedora')
       generate('active_fedora:config:solr')
     end
   end
