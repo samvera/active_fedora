55ae4667a8160c2d2fc835d86ce2f342f16cf47f
Justin Coyne <justin@curationexperts.com>
2015-01-05 11:16:17 -0600

    Update the model generator. Fixes #665

--- lib/generators/active_fedora/model/model_generator.rb
+++ lib/generators/active_fedora/model/model_generator.rb
@@ -5,16 +5,18 @@ module ActiveFedora
     source_root ::File.expand_path('../templates', __FILE__)
     check_class_collision
 
-    class_option :directory, :type => :string, :default => 'models', :desc => "Which directory to generate? (i.e. app/DIRECTORY)"
-    class_option :datastream_directory, :type => :string, :default => 'models/datastreams', :desc => "Which datastream directory to generate? (i.e. models/datastreams)"
-    class_option :has_file_datastream, :type => :string, :default => nil, :desc => "Name a file datastream to create"
-    class_option :descMetadata, :type => :string, :default => nil, :desc => "Add a descMetadata metadata datastream"
+    class_option :directory, type: :string, default: 'models', desc: "Which directory to generate? (i.e. app/DIRECTORY)"
+    class_option :datastream_directory, type: :string, default: 'models/datastreams', desc: "Which datastream directory to generate? (i.e. models/datastreams)"
+    class_option :contains, type: :string, default: nil, desc: "Name a file to attach"
+    class_option :datastream, type: :string, default: nil, desc: "Name a metadata datastream to create"
 
     def install
       template('model.rb.erb', ::File.join('app', directory, "#{file_name}.rb"))
-      template('datastream.rb.erb', ::File.join('app', datastream_directory, "#{file_name}_metadata.rb"))
       template('model_spec.rb.erb', ::File.join('spec', directory, "#{file_name}_spec.rb"))
-      template('datastream_spec.rb.erb', ::File.join('spec', datastream_directory, "#{file_name}_metadata_spec.rb"))
+      if options[:datastream]
+        template('datastream.rb.erb', ::File.join('app', datastream_directory, "#{file_name}_metadata.rb"))
+        template('datastream_spec.rb.erb', ::File.join('spec', datastream_directory, "#{file_name}_metadata_spec.rb"))
+      end
     end
 
     protected
