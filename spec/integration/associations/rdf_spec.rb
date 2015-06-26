05682149059881434947ca637ac3ce04b11a1bea
Justin Coyne <justin@curationexperts.com>
2015-06-26 11:57:57 -0400

    Avoid unnecessary solr query If only one property has been defined for a predicate, there's no reason to run a query to filter the results by class. This avoids a call to solr that is unnecessary in most situations.

---
+++ spec/integration/associations/rdf_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+
+describe "rdf associations" do
+  context "when there is one relationship for the predicate" do
+    before do
+      class Foo < ActiveFedora::Base
+      end
+      class Library < ActiveFedora::Base
+        has_and_belongs_to_many :foos, predicate: ::RDF::URI('http://example.com')
+      end
+    end
+    after do
+      Object.send(:remove_const, :Foo)
+      Object.send(:remove_const, :Library)
+    end
+
+    let(:library) { Library.new }
+
+    it "doesn't not bother to filter by class type" do
+      expect(library.association(:foo_ids)).not_to receive(:filter_by_class)
+      library.foos.to_a
+    end
+  end
+
+  context "when two relationships have the same predicate" do
+    before do
+      class Foo < ActiveFedora::Base
+      end
+      class Bar < ActiveFedora::Base
+      end
+      class Library < ActiveFedora::Base
+        has_and_belongs_to_many :foos, predicate: ::RDF::URI('http://example.com')
+        has_and_belongs_to_many :bars, predicate: ::RDF::URI('http://example.com')
+      end
+    end
+    after do
+      Object.send(:remove_const, :Foo)
+      Object.send(:remove_const, :Bar)
+      Object.send(:remove_const, :Library)
+    end
+
+    let(:library) { Library.new }
+
+    it "filters by class type" do
+      expect(library.association(:foo_ids)).to receive(:filter_by_class).and_call_original
+      library.foos.to_a
+    end
+  end
+end
