a216b07b3539371a73a20f20a439e9f5ef4db9ce
Justin Coyne <justin@curationexperts.com>
2015-06-30 16:21:13 -0500

    Allow the FixityService to accept an RDF::URI

--- spec/unit/fixity_service_spec.rb
+++ spec/unit/fixity_service_spec.rb
@@ -2,32 +2,44 @@ require 'spec_helper'
 
 describe ActiveFedora::FixityService do
 
-  subject { ActiveFedora::FixityService.new("http://path/to/resource") }
+  let(:service) { described_class.new(uri) }
+  let(:uri) { RDF::URI("http://path/to/resource") }
 
-  it { is_expected.to respond_to(:target) }
-  it { is_expected.to respond_to(:response) }
+  describe "the instance" do
+    subject { described_class.new(uri) }
+    it { is_expected.to respond_to(:response) }
+  end
+
+  describe "initialize" do
+    context "with a string" do
+      let(:uri) { 'http://path/to/resource' }
+      subject { service.target }
+      it { is_expected.to eq 'http://path/to/resource' }
+    end
+
+    context "with an RDF::URI" do
+      subject { service.target }
+      it { is_expected.to eq 'http://path/to/resource' }
+    end
+  end
 
   describe "#check" do
     before do
-      expect(subject).to receive(:get_fixity_response_from_fedora).and_return(response)
+      allow(service).to receive(:get_fixity_response_from_fedora).and_return(response)
     end
+    subject { service.check }
     context "with a passing result" do
       let(:response) do
         instance_double("Response", body: '<subject> <http://fedora.info/definitions/v4/repository#status> "SUCCESS"^^<http://www.w3.org/2001/XMLSchema#string> .')
       end
-      specify "returns true" do
-        expect(subject.check).to be true
-      end
-
+      it { is_expected.to be true }
     end
 
     context "with a failing result" do
       let(:response) do
         instance_double("Response", body: '<subject> <http://fedora.info/definitions/v4/repository#status> "BAD_CHECKSUM"^^<http://www.w3.org/2001/XMLSchema#string> .')
       end
-      specify "returns false" do
-        expect(subject.check).to be false
-      end
+      it { is_expected.to be false }
     end
   end
 
