e78160c072fbc1f2b0a002d4bea0acb234e26028
Justin Coyne <jcoyne@justincoyne.com>
2015-09-25 12:12:14 -0500

    Don't skip reject_if when _destroy is passed
    Nested attributes was previously skiping the check of the reject_if proc
    if the _destroy flag was passed. This was occuring when allow_destroy
    was false, so the record was getting created. Thanks to @ojlyytinen for
    finding this bug and the orignal work on PR #879 from which this PR is
    inspired.

--- spec/integration/nested_attribute_spec.rb
+++ spec/integration/nested_attribute_spec.rb
@@ -18,37 +18,9 @@ describe "NestedAttribute behavior" do
       has_many :bars, predicate: ActiveFedora::RDF::Fcrepo::RelsExt.hasMember
       accepts_nested_attributes_for :bars, allow_destroy: true
     end
-
-    # class used in test for reject_if: :all_blank
-    class CarAllBlank < Car
-      accepts_nested_attributes_for :bars, reject_if: :all_blank
-    end
-
-    # class used in test for reject_if: with proc object
-    class CarProc < Car
-      accepts_nested_attributes_for :bars, reject_if: proc { |attributes| attributes['uno'].blank? }
-    end
-
-    # class used in test for reject_if: with method name as symbol
-    class CarSymbol < Car
-      accepts_nested_attributes_for :bars, reject_if: :uno_blank
-
-      def uno_blank(attributes)
-        attributes['uno'].blank?
-      end
-    end
-
-    # class used in test for :limit
-    class CarWithLimit < Car
-      accepts_nested_attributes_for :bars, limit: 1
-    end
   end
   after do
     Object.send(:remove_const, :Bar)
-    Object.send(:remove_const, :CarAllBlank)
-    Object.send(:remove_const, :CarProc)
-    Object.send(:remove_const, :CarSymbol)
-    Object.send(:remove_const, :CarWithLimit)
     Object.send(:remove_const, :Car)
   end
 
@@ -76,8 +48,15 @@ describe "NestedAttribute behavior" do
     expect(bars).to_not include(bar2)
   end
 
-  describe "reject_if: :all_blank" do
+  context "when reject_if: :all_blank" do
+    before do
+      class CarAllBlank < Car
+        accepts_nested_attributes_for :bars, reject_if: :all_blank
+      end
+    end
+
     let(:car_class) { CarAllBlank }
+    after { Object.send(:remove_const, :CarAllBlank) }
     it "should reject attributes when all blank" do
       expect(car.bars.count).to eq 2
       car.update bars_attributes: [{}, {id: bar1.id, uno: "bar1 uno"}]
@@ -88,29 +67,70 @@ describe "NestedAttribute behavior" do
     end
   end
 
-  describe "reject_if: with a proc" do
-    let(:car_class) { CarProc }
-    it "should reject attributes based on proc" do
-      car.update bars_attributes: [{}, {id: bar1.id, uno: "bar1 uno"}, {id: bar2.id, dos: "bar2 dos"}]
-      bar1.reload
-      bar2.reload
-      expect(bar1.uno).to eq "bar1 uno"
-      expect(bar2.dos).to be_nil
+  context "when reject_if attribute is supplied with a proc" do
+    before do
+      class CarProc < Car
+        accepts_nested_attributes_for :bars, reject_if: ->(attributes) { attributes['uno'].blank? }
+      end
     end
-  end
 
-  describe "allow_destroy: false" do
+    after { Object.send(:remove_const, :CarProc) }
     let(:car_class) { CarProc }
-    it "should create a new record even if _destroy is set" do
-      expect(car.bars.count).to eq 2
-      car.update bars_attributes: [{uno: "new uno", _destroy: "1"}]
-      expect(car.bars(true).count).to eq 3
+
+    context "and the reject_if proc evaluates to false" do
+      before do
+        car.update bars_attributes: [{}, { id: bar1.id, uno: "bar1 uno" }]
+      end
+      it "updates attributes" do
+        expect(bar1.reload.uno).to eq "bar1 uno"
+      end
+    end
+
+    context "and the reject_if proc evaluates to true" do
+      before do
+        car.update bars_attributes: [{}, { id: bar1.id, dos: "bar1 uno" }]
+      end
+      it "rejects attributes" do
+        expect(bar1.reload.dos).to be_nil
+      end
+    end
+
+    context "and `allow_destroy: false`" do
+      context "and the reject_if proc evaluates to true" do
+        before do
+          car.update bars_attributes: [{}, { id: bar1.id, dos: "bar1 uno", _destroy: "1" }]
+        end
+        it "rejects attributes (_destroy doesn't affect the outcome)" do
+          expect(bar1.reload.dos).to be_nil
+        end
+      end
+
+      context "a record with the destroy flag and without an id" do
+        it "creates a new record" do
+          expect {
+            car.update bars_attributes: [{ uno: "bar1 uno", _destroy: "1" }]
+          }.not_to change { car.bars(true).count }
+        end
+      end
     end
   end
 
   describe "reject_if: with a symbol" do
+    before do
+      # class used in test for reject_if: with method name as symbol
+      class CarSymbol < Car
+        accepts_nested_attributes_for :bars, reject_if: :uno_blank
+
+        def uno_blank(attributes)
+          attributes['uno'].blank?
+        end
+      end
+    end
+
+    after { Object.send(:remove_const, :CarSymbol) }
     let(:car_class) { CarSymbol }
-    it "should reject attributes base on method name" do
+
+    it "rejects attributes based on method name" do
       car.update bars_attributes: [{}, {id: bar1.id, uno: "bar1 uno"}, {id: bar2.id, dos: "bar2 dos"}]
       bar1.reload
       bar2.reload
@@ -119,7 +139,31 @@ describe "NestedAttribute behavior" do
     end
   end
 
+  describe "allow_destroy: true" do
+    before do
+      class CarDestroy < Car
+        accepts_nested_attributes_for :bars, allow_destroy: true
+      end
+    end
+
+    let(:car_class) { CarDestroy }
+    after { Object.send(:remove_const, :CarDestroy) }
+
+    it "doesn't create a new record if _destroy is set" do
+      expect {
+        car.update bars_attributes: [{ uno: "new uno", _destroy: "1" }]
+      }.not_to change { car.bars(true).count }
+    end
+  end
+
   describe "limit" do
+    before do
+      class CarWithLimit < Car
+        accepts_nested_attributes_for :bars, limit: 1
+      end
+    end
+    after { Object.send(:remove_const, :CarWithLimit) }
+
     let(:car_class) { CarWithLimit }
     it "should throw TooManyRecords" do
       expect {
