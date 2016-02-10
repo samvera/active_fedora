2837f5c16641b6a4ad978ccdb59d7b62fbe3eedb
David Chandek-Stark <dchandekstark@gmail.com>
2016-02-10 16:27:59 -0500

    Adds SSL options to config (closes #985)
    Faraday may require SSL options to work properly.  New code reads
    optional :ssl key in fedora.yml, expecting one or two subkeys, :verify
    and :ca_path.  If SSL options are present in the config, they are passed
    to the Faraday connection contructor. See the Faraday docs for details.

---
+++ spec/unit/fedora_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe ActiveFedora::Fedora do
+  subject { described_class.new(config) }
+  describe "#authorized_connection" do
+    describe "with SSL options" do
+      let(:config) {
+        { url: "https://example.com",
+          user: "fedoraAdmin",
+          password: "fedoraAdmin",
+          ssl: { ca_path: '/path/to/certs' }
+        }
+      }
+      specify {
+        expect(Faraday).to receive(:new).with("https://example.com", ssl: { ca_path: '/path/to/certs' }).twice.and_call_original
+        subject.authorized_connection
+      }
+    end
+  end
+end
