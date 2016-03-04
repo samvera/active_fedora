1a9949e70656a0b77ec82858a0bdc7b802c26653
Chris Beer <cabeer@stanford.edu>
2016-03-04 14:57:59 -0800

    Update rubocop

--- spec/integration/scoped_query_spec.rb
+++ spec/integration/scoped_query_spec.rb
@@ -39,7 +39,7 @@ describe ActiveFedora::Querying do
         result = ModelIntegrationSpec::Basic.all.to_a
         expect(result).to be_instance_of(Array)
         # this test is meaningless if the array length is zero
-        expect(result.length > 0).to be true
+        expect(result.length).to be > 0
         expect(result).to all(be_an(ModelIntegrationSpec::Basic))
       end
     end
