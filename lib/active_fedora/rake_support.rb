196f17909e02d2de27a3a4855f49d1d1d0302049
Esmé Cowles <escowles@ticklefish.org>
2016-05-12 10:03:48 -0400

    Using generated config files for fedora & solr

--- lib/active_fedora/rake_support.rb
+++ lib/active_fedora/rake_support.rb
@@ -4,17 +4,13 @@ def with_test_server(&block)
   with_server('test', &block)
 end
 
-def with_server(environment, fcrepo_port: nil, solr_port: nil)
+def with_server(environment)
   return yield if ENV["#{environment}_SERVER_STARTED"]
 
   ENV["#{environment}_SERVER_STARTED"] = 'true'
 
-  # setting port: nil assigns a random port.
-  solr_params = { port: solr_port, verbose: true, managed: true }
-  fcrepo_params = { port: fcrepo_port, verbose: true, managed: true,
-                    enable_jms: false, fcrepo_home_dir: "fcrepo4-#{environment}-data" }
-  SolrWrapper.wrap(solr_params) do |solr|
-    ENV["SOLR_#{environment.upcase}_PORT"] = solr.port
+  SolrWrapper.wrap(load_config(:solr, environment)) do |solr|
+    ENV["SOLR_#{environment.upcase}_PORT"] = solr.port.to_s
     solr_config_path = File.join('solr', 'config')
     # Check to see if configs exist in a path relative to the working directory
     unless Dir.exist?(solr_config_path)
@@ -23,11 +19,19 @@ def with_server(environment, fcrepo_port: nil, solr_port: nil)
       solr_config_path = File.join(File.expand_path("../..", File.dirname(__FILE__)), "solr", "config")
     end
     solr.with_collection(name: "hydra-#{environment}", dir: solr_config_path) do
-      FcrepoWrapper.wrap(fcrepo_params) do |fcrepo|
-        ENV["FCREPO_#{environment.upcase}_PORT"] = fcrepo.port
+      FcrepoWrapper.wrap(load_config(:fcrepo, environment)) do |fcrepo|
+        ENV["FCREPO_#{environment.upcase}_PORT"] = fcrepo.port.to_s
         yield
       end
     end
   end
   ENV["#{environment}_SERVER_STARTED"] = 'false'
+end
+
+private
+
+def load_config(service, environment)
+  config_file = environment == 'test' ? "config/#{service}_wrapper_test.yml" : ".#{service}_wrapper"
+  return { config: config_file } if File.exist?(config_file)
+  {}
 end
