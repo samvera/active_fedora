1faf9b95bdb80c8105df2ed7aca13dfb6ee26db6
Chris Colvard <cjcolvar@indiana.edu>
2016-11-14 11:36:15 -0500

    Add base_uri convenience method
    Conflicts:
    	spec/unit/core_spec.rb
    	spec/unit/file_spec.rb

--- spec/unit/core/fedora_id_translator_spec.rb
+++ spec/unit/core/fedora_id_translator_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe ActiveFedora::Core::FedoraIdTranslator do
   describe ".call" do
     let(:result) { described_class.call(id) }
     context "when given an id" do
-      let(:good_uri) { ActiveFedora.fedora.host + ActiveFedora.fedora.base_path + "/banana" }
+      let(:good_uri) { ActiveFedora.fedora.base_uri + "/banana" }
       let(:id) { "banana" }
       it "returns a fedora URI" do
         expect(result).to eq good_uri
@@ -18,7 +18,7 @@ RSpec.describe ActiveFedora::Core::FedoraIdTranslator do
       end
 
       context "with characters that need escaping" do
-        let(:good_uri) { ActiveFedora.fedora.host + ActiveFedora.fedora.base_path + "/%5Bfrob%5D" }
+        let(:good_uri) { ActiveFedora.fedora.base_uri + "/%5Bfrob%5D" }
         let(:id) { "[frob]" }
         it "returns a good fedora URI" do
           expect(result).to eq good_uri
