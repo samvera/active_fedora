4691b552e707edd176c822023148ced2f7a0d802
Chris Beer <chris@cbeer.info>
2016-11-17 09:10:02 -0800

    Don't whitelist rubydora connection options

--- spec/unit/rubydora_connection_spec.rb
+++ spec/unit/rubydora_connection_spec.rb
@@ -3,11 +3,10 @@ require 'active_fedora/rubydora_connection'
 
 describe ActiveFedora::RubydoraConnection do
   describe 'initialize' do
-    it "should pass through valid options" do
-      @instance = ActiveFedora::RubydoraConnection.new :timeout => 3600, :fake_option => :missing, :force => true, :validateChecksum=>true
+    it "should pass through options" do
+      @instance = ActiveFedora::RubydoraConnection.new :timeout => 3600, :force => true, :validateChecksum=>true
       expect(@instance.connection.client.options[:timeout]).to eq(3600)
       expect(@instance.connection.config[:validateChecksum]).to eq(true)
-      expect(@instance.connection.client.options.has_key?(:fake_option)).to be_falsey
     end
   end
 end
