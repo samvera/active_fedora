8dd36b2d90847d31e5091d0ae2ba22592440af30
Michael J. Giarlo <mjgiarlo@stanford.edu>
2018-03-12 15:11:17 -0700

    Prefer om_term_values to send when solrizing within SimpleDatastream
    This allows downstream users to use fields that conflict with public AF methods. Case in point: I need to support a field named "prefix" which entered the public API around AF 7.0.0.

--- spec/unit/simple_datastream_spec.rb
+++ spec/unit/simple_datastream_spec.rb
@@ -3,26 +3,40 @@ require 'spec_helper'
 describe ActiveFedora::SimpleDatastream do
 
   before do
-    @sample_xml =  "<fields><coverage>coverage1</coverage><coverage>coverage2</coverage><creation_date>2012-01-15</creation_date><mydate>fake-date</mydate><publisher>publisher1</publisher></fields>"
+    @sample_xml =  "<fields><coverage>coverage1</coverage><coverage>coverage2</coverage><creation_date>2012-01-15</creation_date><mydate>fake-date</mydate><prefix>foobar</prefix><publisher>publisher1</publisher></fields>"
     @test_ds = ActiveFedora::SimpleDatastream.from_xml(@sample_xml )
     @test_ds.field :coverage
     @test_ds.field :creation_date, :date
     @test_ds.field :mydate
+    @test_ds.field :prefix
     @test_ds.field :publisher
-
   end
+
   it "from_xml should parse everything correctly" do
     expect(@test_ds.ng_xml).to be_equivalent_to @sample_xml
   end
 
-  
+  describe "#to_solr" do
+    it "should work as expected" do
+      expect(@test_ds.to_solr).to eq(
+        {
+          'coverage_tesim' => ['coverage1', 'coverage2'],
+          'creation_date_dtsim' => '2012-01-15',
+          'mydate_tesim' => 'fake-date',
+          'prefix_tesim' => 'foobar',
+          'publisher_tesim' => 'publisher1'
+        }
+      )
+    end
+  end
+
   describe '#new' do
-    describe "model methods" do 
+    describe "model methods" do
 
       [:coverage, :mydate, :publisher].each do |el|
         it "should respond to getters and setters for the string typed #{el} element" do
           value = "Hey #{el}"
-          @test_ds.send("#{el.to_s}=", value) 
+          @test_ds.send("#{el.to_s}=", value)
           expect(@test_ds.send(el).first).to eq(value)  #Looking at first because creator has 2 nodes
         end
       end
@@ -34,7 +48,7 @@ describe ActiveFedora::SimpleDatastream do
       end
     end
   end
-  
+
   describe '.to_xml' do
     it 'should output the fields hash as Qualified Dublin Core XML' do
       @test_ds.publisher= "charlie"
@@ -46,6 +60,7 @@ describe ActiveFedora::SimpleDatastream do
           <coverage>20%</coverage>
           <creation_date>2012-01-15</creation_date>
           <mydate>fake-date</mydate>
+          <prefix>foobar</prefix>
           <publisher>charlie</publisher>
         </fields>')
     end
