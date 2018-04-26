bc2501f30327d34cf3234fe5fe63b98c7f0c71e4
Chris Beer <cabeer@stanford.edu>
2018-04-25 18:25:25 -0700

    Update ActiveFedora for compatibility with Rails 5.1

--- lib/active_fedora/om_datastream.rb
+++ lib/active_fedora/om_datastream.rb
@@ -6,7 +6,7 @@ module ActiveFedora
     before_save do
       if content.blank?
         ActiveFedora::Base.logger.warn "Cowardly refusing to save a datastream with empty content: #{self.inspect}" if ActiveFedora::Base.logger
-        false
+        throw(:abort)
       end
     end
 
