469ed4ac65a41f3b376fe311007ef6842d4fbc5f
Trey Pendragon <tpendragon@princeton.edu>
2019-10-17 20:09:54 -0700

    Fix containers.

--- lib/active_fedora/containers/indirect_container.rb
+++ lib/active_fedora/containers/indirect_container.rb
@@ -1,9 +1,5 @@
 module ActiveFedora
   class IndirectContainer < Container
     property :inserted_content_relation, predicate: ::RDF::Vocab::LDP.insertedContentRelation
-
-    def build_ldp_resource(id)
-      IndirectContainerResource.new(ActiveFedora.fedora.connection, self.class.id_to_uri(id))
-    end
   end
 end
