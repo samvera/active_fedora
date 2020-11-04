64c08f51b872b7f933e6136b9c81de8f2596514f
Stuart Kenny <skenny@tchpc.tcd.ie>
2020-11-04 12:20:10 +0000

    Update for versionable resources, no API for restoring versions

--- lib/active_fedora/versionable.rb
+++ lib/active_fedora/versionable.rb
@@ -6,21 +6,11 @@ module ActiveFedora
       class_attribute :versionable
     end
 
-    def model_type
-      if respond_to?(:metadata)
-        metadata.ldp_source.graph.query(predicate: ::RDF.type).objects
-      else
-        resource.query(subject: resource.rdf_subject, predicate: ::RDF.type).objects
-      end
-    end
-
     # Returns an array of ActiveFedora::VersionsGraph::ResourceVersion objects.
-    # Excludes auto-snapshot versions from Fedora.
     def versions(reload = false)
       response = versions_request
 
       return ActiveFedora::VersionsGraph.new unless response
-
       if reload
         @versions = ActiveFedora::VersionsGraph.new << versions_request.reader
       else
@@ -29,44 +19,30 @@ module ActiveFedora
     end
 
     def create_version
-      resp = ActiveFedora.fedora.connection.post(versions_uri, nil, slug: version_name)
+      resp = ActiveFedora.fedora.connection.post(versions_uri, nil)
       @versions = nil
       resp.success?
     end
 
     # Queries Fedora to figure out if there are versions for the resource.
     def has_versions?
-      ActiveFedora.fedora.connection.head(versions_uri)
-      true
+      resp = ActiveFedora.fedora.connection.get(versions_uri)
+      graph = ::RDF::Graph.new << resp.reader
+      !graph.query(predicate: ::RDF::Vocab::LDP.contains).blank?
     rescue Ldp::NotFound
       false
-    end
-
-    def restore_version(label)
-      resp = ActiveFedora.fedora.connection.patch(versions.with_label(label).uri, nil)
-      @versions = nil
-      reload
-      refresh_attributes if respond_to?("refresh_attributes")
-      resp.success?
     end
 
     private
 
       def versions_request
-        return false unless has_versions?
         ActiveFedora.fedora.connection.get(versions_uri)
+      rescue Ldp::NotFound
+        false
       end
 
       def versions_uri
         uri + '/fcr:versions'
-      end
-
-      def version_name
-        if versions.all.empty?
-          "version1"
-        else
-          "version" + (versions.all.count + 1).to_s
-        end
       end
 
       def status_message(response)
--- spec/integration/versionable_spec.rb
+++ spec/integration/versionable_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'time'
 
 describe ActiveFedora::Versionable do
   context "For ActiveFedora::Base" do
@@ -38,16 +39,16 @@ describe ActiveFedora::Versionable do
         allow(graph).to receive(:fedora_versions) { versions }
       end
 
-      let(:version1) { instance_double(ActiveFedora::VersionsGraph::ResourceVersion, uri: 'http://localhost:8983/fedora/rest/test/84/61/63/98/84616398-f63a-4572-ba01-0689339e4fcb/fcr:versions/87a0a8c317f1e711aa993d-e1d2-4a65-93ee-3a12fc9541ab', label: 'version1', created: '2015-04-02T19:54:45.962Z') }
-      let(:version2) { instance_double(ActiveFedora::VersionsGraph::ResourceVersion, uri: 'http://localhost:8983/fedora/rest/test/84/61/63/98/84616398-f63a-4572-ba01-0689339e4fcb/fcr:versions/87a0a8c317f1e790373a67-c9ee-447d-b740-4faa882b1a1f', label: 'version2', created: '2015-04-02T19:54:45.96Z') }
+      let(:version1) { instance_double(ActiveFedora::VersionsGraph::ResourceVersion, uri: 'http://localhost:8983/fedora/rest/test/84/61/63/98/84616398-f63a-4572-ba01-0689339e4fcb/fcr:versions/20180518230244', created: "20180518230244") }
+      let(:version2) { instance_double(ActiveFedora::VersionsGraph::ResourceVersion, uri: 'http://localhost:8983/fedora/rest/test/84/61/63/98/84616398-f63a-4572-ba01-0689339e4fcb/fcr:versions/20180518230544', created: "20180518230544") }
       let(:versions) { [version1, version2] }
 
       it 'sorts by DateTime' do
-        expect(graph.first).to eq version2
+        expect(graph.last).to eq version2
       end
 
       context 'with an unparseable created date' do
-        let(:version2) { instance_double(ActiveFedora::VersionsGraph::ResourceVersion, uri: 'http://localhost:8983/fedora/rest/test/84/61/63/98/84616398-f63a-4572-ba01-0689339e4fcb/fcr:versions/87a0a8c317f1e790373a67-c9ee-447d-b740-4faa882b1a1f', label: 'version2', created: '') }
+        let(:version2) { instance_double(ActiveFedora::VersionsGraph::ResourceVersion, uri: 'http://localhost:8983/fedora/rest/test/84/61/63/98/84616398-f63a-4572-ba01-0689339e4fcb/fcr:versions/version2', created: '') }
 
         it 'raises an exception' do
           expect { graph.first }.to raise_error(ActiveFedora::VersionLacksCreateDate)
@@ -60,7 +61,7 @@ describe ActiveFedora::Versionable do
           allow(version2).to receive(:created) { raise NoMethodError }
         end
 
-        let(:version2) { instance_double(ActiveFedora::VersionsGraph::ResourceVersion, uri: 'http://localhost:8983/fedora/rest/test/84/61/63/98/84616398-f63a-4572-ba01-0689339e4fcb/fcr:versions/87a0a8c317f1e790373a67-c9ee-447d-b740-4faa882b1a1f', label: 'version2') }
+        let(:version2) { instance_double(ActiveFedora::VersionsGraph::ResourceVersion, uri: 'http://localhost:8983/fedora/rest/test/84/61/63/98/84616398-f63a-4572-ba01-0689339e4fcb/fcr:versions') }
 
         it 'raises an exception' do
           expect { graph.first }.to raise_error(ActiveFedora::VersionLacksCreateDate)
@@ -80,12 +81,12 @@ describe ActiveFedora::Versionable do
       it "has one version" do
         expect(object.versions).to be_kind_of ActiveFedora::VersionsGraph
         expect(object.versions.all.first).to be_kind_of ActiveFedora::VersionsGraph::ResourceVersion
-        expect(object.versions.all.first.label).to eql "version1"
         expect(object.versions.all.first.created).to start_with current_year
       end
 
       context "two times" do
         before do
+          sleep(1)
           object.title = ["Surrender and prepare to be boarded"]
           object.save
           object.create_version
@@ -94,32 +95,7 @@ describe ActiveFedora::Versionable do
         it "has two versions" do
           expect(object.versions.all.size).to eq 2
           object.versions.all.each_index do |index|
-            expect(object.versions.all[index].label).to end_with "version" + (index + 1).to_s
             expect(object.versions.all[index].created).to start_with current_year
-          end
-        end
-
-        context "then restoring" do
-          let(:first_version) { "version1" }
-          before do
-            object.restore_version(first_version)
-          end
-
-          it "will return to the first version's values" do
-            expect(object.title).to eq(["Greetings Earthlings"])
-          end
-
-          context "and creating additional versions" do
-            before do
-              object.title = ["Now, surrender and prepare to be boarded"]
-              object.save!
-              object.create_version
-            end
-
-            it "has three versions" do
-              expect(object.versions.all.size).to eq 3
-              expect(object.title).to eq(["Now, surrender and prepare to be boarded"])
-            end
           end
         end
       end
@@ -157,6 +133,7 @@ describe ActiveFedora::Versionable do
       context "after creating the file" do
         let(:first_file) { File.new(File.join(File.dirname(__FILE__), "../fixtures/dino.jpg")) }
         let(:first_name) { "dino.jpg" }
+
         before do
           content.content = first_file
           content.original_name = first_name
@@ -164,15 +141,10 @@ describe ActiveFedora::Versionable do
           content.create_version
         end
 
-        it "links to versions endpoint" do
-          expect(content.metadata.ldp_source.graph.query(predicate: ::RDF::Vocab::Fcrepo4.hasVersions).objects).to_not be_empty
-        end
-
         it "has one version" do
           expect(content.versions.all.size).to eq 1
           expect(content.original_name).to eql(first_name)
           expect(content.content.size).to eq first_file.size
-          expect(content.versions.first.label).to eql "version1"
           expect(content.versions.first.created).to start_with current_year
         end
 
@@ -180,6 +152,7 @@ describe ActiveFedora::Versionable do
           let(:second_file) { File.new(File.join(File.dirname(__FILE__), "../fixtures/minivan.jpg")) }
           let(:second_name) { "minivan.jpg" }
           before do
+            sleep(1)
             content.content = second_file
             content.original_name = second_name
             content.save
@@ -191,7 +164,6 @@ describe ActiveFedora::Versionable do
             expect(content.original_name).to eql(second_name)
             expect(content.content.size).to eq second_file.size
             content.versions.all.each_index do |index|
-              expect(content.versions.all[index].label).to end_with "version" + (index + 1).to_s
               expect(content.versions.all[index].created).to start_with current_year
             end
           end
@@ -208,44 +180,6 @@ describe ActiveFedora::Versionable do
             it "reports on the fixity of each version" do
               results.each do |result|
                 expect(result).to be true
-              end
-            end
-          end
-
-          context "then restoring" do
-            let(:first_version) { "version1" }
-            before do
-              content.restore_version(first_version)
-            end
-
-            it "stills have two unique versions" do
-              expect(content.versions.all.size).to eq 2
-            end
-
-            it "loads the restored file's content" do
-              expect(content.content.size).to eq first_file.size
-            end
-
-            it "loads the restored file's original name" do
-              expect(content.original_name).to eql(first_name)
-            end
-
-            context "and creating additional versions" do
-              before do
-                content.content = first_file
-                content.original_name = first_name
-                content.save
-                content.create_version
-              end
-
-              it "has three unique versions" do
-                expect(content.versions.all.size).to eq 3
-                expect(content.original_name).to eql(first_name)
-                expect(content.content.size).to eq first_file.size
-                content.versions.all.each_index do |index|
-                  expect(content.versions.all[index].label).to end_with "version" + (index + 1).to_s
-                  expect(content.versions.all[index].created).to start_with current_year
-                end
               end
             end
           end
