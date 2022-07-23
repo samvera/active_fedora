4bd8f050363dcd5f3721c83f04fec15e3034d246
Trey Pendragon <tpendragon@princeton.edu>
2022-07-23 14:08:33 -0700

    Add 6.1 support

--- spec/support/an_active_model.rb
+++ spec/support/an_active_model.rb
@@ -7,7 +7,7 @@ shared_examples_for "An ActiveModel" do
     expect(inspected_object).to be_kind_of(klass)
   end
 
-  def assert_equal(the_other, one)
+  def assert_equal(the_other, one, description = nil)
     expect(one).to eq the_other
   end
 
